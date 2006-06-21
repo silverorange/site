d505ed03aa23e96c07a381e42e851e841ac08691
Nathan Fredrickson <nathan@silverorange.com>
2006-06-21 16:39:16 +0000

    New exception for cookies.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@7312 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Site/exceptions/SiteCookieException.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Site/exceptions/SiteException.php';
+
+/**
+ * Thrown when dealing with cookies
+ *
+ * @package   Site
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteCookieException extends SiteException
+{
+}
+
+?>
