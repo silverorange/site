5d0991b21a4c2598d1342e88d59342a58fb7d200
Nick Burka <nick@silverorange.com>
2006-06-22 01:54:43 +0000

    Simpler more generic error message
    svn commit r7338

--- Site/pages/SiteExceptionPage.php
+++ Site/pages/SiteExceptionPage.php
@@ -58,9 +58,8 @@ class SiteExceptionPage extends SitePage
 		printf('<p>%s</p>', $this->getSummary($status));
 
 		echo '<ul class="spaced">
-				<li>If you followed a link from our site or elsewhere, please <a href="about/contact">contact us</a> and let us know where you came from so we can do our best to fix it.</li>
+				<li>If you followed a link from our site or elsewhere, please contact us and let us know where you came from so we can do our best to fix it.</li>
 				<li>If you typed in the address, please double check the spelling.</li>
-				<li>If you are looking for a product or product information, try browsing the product listing to the left or using the search box on the top right.</li>
 			</ul>';
 
 		if ($this->exception !== null)
