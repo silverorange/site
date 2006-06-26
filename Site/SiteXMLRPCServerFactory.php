f5ef0d69ab0f574b35aaaa2ac31583afed2c0bc1
Nathan Fredrickson <nathan@silverorange.com>
2006-06-26 19:58:30 +0000

    Push up from Store to Site.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@7537 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Site/SiteXMLRPCServerFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'Site/exceptions/SiteNotFoundException.php';
+require_once 'Site/layouts/SiteXMLRPCServerLayout.php';
+require_once 'Site/SitePageFactory.php';
+
+/**
+ * @package   Site
+ * @copyright 2006 silverorange
+ */
+abstract class SiteXMLRPCServerFactory extends SitePageFactory
+{
+	// {{{ public function resolvePage()
+
+	public function resolvePage($app, $source)
+	{
+		$layout = $this->resolveLayout($app, $source);
+		$map = $this->getPageMap();
+
+		if (isset($map[$source])) {
+			$class = $map[$source];
+			$params = array($app, $layout);
+			$page = $this->instantiatePage($class, $params);
+			return $page;
+		}
+
+		throw new SiteNotFoundException();
+	}
+
+	// }}}
+	// {{{ protected function resolveLayout()
+
+	protected function resolveLayout($app, $source)
+	{
+		return new SiteXMLRPCServerLayout($app);
+	}
+
+	// }}}
+}
+
+?>
