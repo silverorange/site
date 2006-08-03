3f8441e8c70c3b19c5b3b636f686a25ac5c768b9
Nathan Fredrickson <nathan@silverorange.com>
2006-08-03 17:39:04 +0000

    Start of a site config module.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@9014 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Site/SiteConfigModule.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'Site/SiteApplicationModule.php';
+
+/**
+ * Web application module for site configuration
+ *
+ * @package   Site
+ * @copyright 2006 silverorange
+ */
+class SiteConfigModule extends SiteApplicationModule
+{
+}
+
+?>
