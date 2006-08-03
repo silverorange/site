d3e6fef8db0d24ef1798d7417ce9799ece4a2cc5
Nathan Fredrickson <nathan@silverorange.com>
2006-08-03 19:30:42 +0000

    New SiteConfigModule based on the ZendFramework Config module.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@9029 94490950-a86c-4493-9ccb-530f72933e47

--- Site/SiteConfigModule.php
+++ Site/SiteConfigModule.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'Zend/Config.php';
+require_once 'Zend/Config/Ini.php';
 require_once 'Site/SiteApplicationModule.php';
 
 /**
@@ -10,6 +12,78 @@ require_once 'Site/SiteApplicationModule.php';
  */
 class SiteConfigModule extends SiteApplicationModule
 {
+	// {{{ private properties
+
+	private $zend_config = null;
+	private $filename;
+	private $section = 'default';
+
+	// }}}
+	// {{{ public function init()
+
+	/**
+	 * Initializes this module
+	 */
+	public function init()
+	{
+		if ($this->zend_config === null)
+			$this->load();
+	}
+
+	// }}}
+	// {{{ public function load()
+
+	/**
+	 * Loads configuration
+	 */
+	public function load()
+	{
+		$this->zend_config = new Zend_Config(
+			Zend_Config_Ini::load($this->filename, $this->section));
+	}
+
+	// }}}
+	// {{{ public function setFilename()
+
+	/**
+	 * Sets the configuration filename
+	 *
+	 * @param string $name the path and name of the confuration file.
+	 */
+	public function setFilename($filename)
+	{
+		$this->filename = $filename;
+	}
+
+	// }}}
+	// {{{ public function setScope()
+
+	/**
+	 * Sets the configuration section
+	 *
+	 * @param string $section the section to load from the config file.
+	 */
+	public function setScope($section)
+	{
+		$this->section = $section;
+	}
+
+	// }}}
+	// {{{ private function __get()
+
+	/**
+	 * Gets a config value
+	 *
+	 * @param string $name the name of the config value to get.
+	 *
+	 * @return mixed the value of the config setting.
+	 */
+	private function __get($name)
+	{
+		return $this->zend_config->$name;
+	}
+
+	// }}}
 }
 
 ?>
