71782d6aa7cdbe1a26a0fd9b4514619ddc4d56e4
Michael Gauthier <mike@silverorange.com>
2006-08-10 14:16:32 +0000

    use SiteSessionModule
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@9418 94490950-a86c-4493-9ccb-530f72933e47

--- Site/SiteMessagesModule.php
+++ Site/SiteMessagesModule.php
@@ -17,28 +17,47 @@ class SiteMessagesModule extends SiteApplicationModule
 {
     // {{{ public function init()
 
+	/**
+	 * Initializes this messages module
+	 *
+	 * If there are no messages defined in the session, the messages are
+	 * defined as an empty list.
+	 */
 	public function init()
 	{
-		if (!isset($_SESSION['messages']) || !is_array($_SESSION['messages']))
-			$_SESSION['messages'] = array();
+		if (!isset($this->app->session->messages) ||
+			!is_array($this->app->session->messages))
+			$this->app->session->messages = array();
 	}
 
     // }}}
     // {{{ public function add()
 
+	/**
+	 * Adds a message to this module
+	 *
+	 * @param SwatMessage $message the message to add.
+	 */
 	public function add(SwatMessage $message)
 	{
-		$_SESSION['messages'][] = $message;
+		$this->app->session->messages[] = $message;
 	}
 
     // }}}
     // {{{ public function getAll()
 
-	public function getAll()
+	/**
+	 * Gets all messages from this module
+	 *
+	 * After this method runs, the messages are cleared from the module.
+	 *
+	 * @return array the array of SwatMessage objects in this module.
+	 */
+	public function &getAll()
 	{
-		$ret = $_SESSION['messages'];
-		$_SESSION['messages'] = array();
-		return $ret;
+		$messages = $this->app->session->messages;
+		$this->app->session->messages = array();
+		return $messages;
 	}
 
     // }}}
