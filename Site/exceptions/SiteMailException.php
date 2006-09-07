0561a4f6d98730a98c1ba910ca901db315fe23e7
Nathan Fredrickson <nathan@silverorange.com>
2006-09-07 16:34:13 +0000

    New exception for mail problems.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@10657 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Site/exceptions/SiteMailException.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Site/exceptions/SiteException.php';
+
+/**
+ * Thrown when sending mail
+ *
+ * @package   Site
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteMailException extends SiteException
+{
+}
+
+?>
