c181f1e42d2bb9174a8d0b0f69bddad0d731872b
Nathan Fredrickson <nathan@silverorange.com>
2006-09-20 01:46:45 +0000

    prepare for release of 1.0.2
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@11124 94490950-a86c-4493-9ccb-530f72933e47

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.0.1';
+$version = '1.0.2';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -50,7 +50,7 @@ $package->addMaintainer('lead', 'gauthierm', 'Mike Gauthier', 'mike@silverorange
 
 $package->setPhpDep('5.1.5');
 $package->setPearinstallerDep('1.4.0');
-$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '0.9.6');
+$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.0.0');
 $package->addPackageDepWithChannel('required', 'Date', 'pear.silverorange.com', '1.4.7so1');
 $package->addPackageDepWithChannel('required', 'MDB2', 'pear.php.net', '2.2.2');
 $package->addPackageDepWithChannel('required', 'Mail', 'pear.php.net', '1.1.10');
