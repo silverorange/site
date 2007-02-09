fa02b68e8a5e7b58faf1f9089237470a75219cf2
Michael Gauthier <mike@silverorange.com>
2007-02-09 16:53:43 +0000

    Make SiteCookieModule use SwatString::signed(Un)?Serialize(). Fixes ticket #578.
    svn commit r14880

--- Site/SiteCookieModule.php
+++ Site/SiteCookieModule.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'Swat/SwatString.php';
 require_once 'Site/SiteApplicationModule.php';
 require_once 'Site/exceptions/SiteCookieException.php';
 
@@ -65,7 +66,7 @@ class SiteCookieModule extends SiteApplicationModule
 		elseif (is_string($expiry))
 			$expiry = strtotime($expiry);
 
-		$cookie_value = $this->saltValue($value);
+		$cookie_value = SwatString::signedSerialize($value, $this->salt);
 
 		// TODO: get domain from application
 		//if ($domain = null)
@@ -117,53 +118,15 @@ class SiteCookieModule extends SiteApplicationModule
 		if (!isset($_COOKIE[$name]))
 			throw new SiteCookieException("Cookie '$name' is not set.");
 
-		$value = $this->unsaltValue($_COOKIE[$name]);
-		return $value;
-	}
-
-	// }}}
-	// {{{ private function saltValue()
-
-	private function saltValue($value)
-	{
-		$package = array();
-		$package['value'] = $value;
-		$package['hash'] = $this->getHash($value);
-		return serialize($package);
-	}
-
-	// }}}
-	// {{{ private function unsaltValue()
-
-	private function unsaltValue($value)
-	{
-		$package = @unserialize($value);
-
-		// serialized value is broken
-		if ($package === false && strcmp($value, serialize(false)) != 0)
-			throw new SiteCookieException('Could not unserialize cookie.');
-
-		// serialized data is not how we expected
-		if (!is_array($package) ||
-			!isset($package['value']) ||
-			!isset($package['hash']))
-				throw new SiteCookieException('Corrupt cookie data.');
-
-		$value = $package['value'];
-
-		// hash does not match, cookie was tampered with
-		if ($package['hash'] !== $this->getHash($value))
-			throw new SiteCookieException('Invalid cookie hash.');
+		try {
+			$value =
+				SwatString::signedUnserialize($_COOKIE[$name], $this->salt);
+		} catch (SwatInvalidSerializedDataException $e) {
+			throw new SiteCookieException(sprintf('Invalid cookie data: %s.',
+				$e->getData()));
+		}
 
 		return $value;
-	}
-
-	// }}}
-	// {{{ private function getHash()
-
-	private function getHash($value)
-	{
-		return md5($this->salt.serialize($value));
 	}
 
 	// }}}
