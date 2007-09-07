010499110425ff83b37d52027f4c1bf4ae94bce9
Nick Burka <nick@silverorange.com>
2007-09-07 19:01:32 +0000

    Add an 'enabled' field for articles in the Site package to allow disabling articles. Store package sites don't need this because they have region bindings that take care of it. I'll commit changes to Store next.
    svn commit r20009

--- sql/tables/Article.sql
+++ sql/tables/Article.sql
@@ -7,6 +7,7 @@ create table Article (
 	createdate timestamp not null default LOCALTIMESTAMP,
 	modified_date timestamp not null default LOCALTIMESTAMP,
 	displayorder int not null default 0,
+	enabled boolean not null default true,
 	show boolean not null default true,
 	searchable boolean not null default true,
 	shortname varchar(255),
@@ -18,5 +19,6 @@ ALTER TABLE Article ADD CONSTRAINT Articlefk FOREIGN KEY (parent) REFERENCES Art
 CREATE INDEX Article_parent_index ON Article(parent);
 CREATE INDEX Article_shortname_index ON Article(shortname);
 CREATE INDEX Article_show_index ON Article(show);
+CREATE INDEX Article_enabled_index ON Article(enabled);
 CREATE INDEX Article_searchable_index ON Article(searchable);
 
