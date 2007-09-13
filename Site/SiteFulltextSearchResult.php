8fb502e5fae4edb2c9c630f69e6780ab67c5b421
Nathan Fredrickson <nathan@silverorange.com>
2007-09-13 19:41:08 +0000

    Add docs
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@20214 94490950-a86c-4493-9ccb-530f72933e47

--- Site/SiteFulltextSearchResult.php
+++ Site/SiteFulltextSearchResult.php
@@ -10,11 +10,27 @@ interface SiteFulltextSearchResult
 {
 	// {{{ public function getJoinClause()
 
+	/**
+	 * Get an SQL join clause to use to query with this fulltext result
+	 *
+	 * @param string $id_field_name name of the id field to join with.
+	 * @param string $type document type to join with.
+	 *
+	 * @return string
+	 */
 	public function getJoinClause($id_field_name, $type);
 
 	// }}}
 	// {{{ public function getOrderByClause()
 
+	/**
+	 * Perform a full text searche and returns the result
+	 *
+	 * @param string $default_clause a default order by clause to use in
+	 *                                addition to the fulltext clause.
+	 *
+	 * @return string
+	 */
 	public function getOrderByClause($default_clause);
 
 	// }}}
