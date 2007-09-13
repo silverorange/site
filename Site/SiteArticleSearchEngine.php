861066545d649f2ec3863de5f1e9d37c0ffe5ed0
Nathan Fredrickson <nathan@silverorange.com>
2007-09-13 20:06:48 +0000

    A search engine for articles.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@20223 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Site/SiteArticleSearchEngine.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once 'Site/SiteSearchEngine.php';
+require_once 'Site/dataobjects/SiteArticleWrapper.php';
+
+/**
+ * An article search engine
+ *
+ * @package   Site
+ * @copyright 2007 silverorange
+ */
+class SiteArticleSearchEngine extends SiteSearchEngine
+{
+	// {{{ protected function getResultWrapperClass()
+
+	protected function getResultWrapperClass()
+	{
+		$wrapper_class = SwatDBClassMap::get('SiteArticleWrapper');
+
+		return $wrapper_class;
+	}
+
+	// }}}
+	// {{{ protected function getSelectClause()
+
+	protected function getSelectClause()
+	{
+		$clause = 'select Article.*';
+
+		return $clause;
+	}
+
+	// }}}
+	// {{{ protected function getFromClause()
+
+	protected function getFromClause()
+	{
+		$clause = 'from Article';
+
+		if ($this->fulltext_result !== null)
+			$clause.= ' '.
+				$this->fulltext_result->getJoinClause(
+					'Article.id', 'article');
+
+		return $clause;
+	}
+
+	// }}}
+	// {{{ protected function getWhereClause()
+
+	protected function getWhereClause()
+	{
+		$clause = sprintf('where Article.searchable = %s',
+			$this->app->db->quote(true, 'boolean'));
+
+		return $clause;
+	}
+
+	// }}}
+	// {{{ protected function getOrderByClause()
+
+	protected function getOrderByClause()
+	{
+		if ($this->fulltext_result === null)
+			$clause = sprintf('order by Article.title');
+		else
+			$clause = 
+				$this->fulltext_result->getOrderByClause('Article.title');
+
+		return $clause;
+	}
+
+	// }}}
+}
+
+?>
