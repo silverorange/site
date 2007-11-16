9d75331e0726c69398740e83128543cec9829993
Michael Gauthier <mike@silverorange.com>
2007-11-16 03:56:44 +0000

    part two of config module refactoring. Add methods to declare setting definitions.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@23189 94490950-a86c-4493-9ccb-530f72933e47

--- Site/SiteConfigModule.php
+++ Site/SiteConfigModule.php
@@ -66,6 +66,16 @@ class SiteConfigModule extends SiteApplicationModule
 	 */
 	private $loaded = false;
 
+	/**
+	 * Configuration setting definitions of this config module
+	 *
+	 * @var array
+	 *
+	 * @see SiteConfigModule::addDefinition()
+	 * @see SiteConfigModule::addDefinitions()
+	 */
+	private $definitions = array();
+
 	// }}}
 	// {{{ public function init()
 
@@ -112,16 +122,101 @@ class SiteConfigModule extends SiteApplicationModule
 	}
 
 	// }}}
-	// {{{ protected function isValidKey()
+	// {{{ public function addDefinitions()
 
 	/**
-	 * Checks whether or not a key name is valid
+	 * Adds multiple configuration setting definitions to this config module
 	 *
-	 * @param string $name the key name to check.
+	 * Only defined settings may be loaded from an ini file. Other settings
+	 * will throw exceptions when they are loaded.
 	 *
-	 * @return boolean true if the key name is valid and false if it is not.
+	 * @param array $definitions an associative array of setting definitions.
+	 *                            The array is of the form:
+	 *                            qualified_name => default_value where
+	 *                            qualified_name is a string containing the
+	 *                            section name and the setting name separated
+	 *                            by a '.'. For example, a setting for 'dsn' in
+	 *                            the 'database' section would have a qualified
+	 *                            name of 'database.dsn'. The default value is
+	 *                            a string containing the value to use for the
+	 *                            setting if the setting does not exist in the
+	 *                            loaded configuration file. Use null to
+	 *                            specify no default.
+	 *
+	 * @throws SiteException if the qualified name does not contain a section
+	 *                       and a setting name.
 	 */
-	protected function isValidKey($name)
+	public function addDefinitions(array $definitions)
+	{
+		foreach ($definitions as $qualified_name => $default_value) {
+			if (strpos('.', $qualified_name) === false) {
+				throw new SiteException(sprintf(
+					"Qualified name of configuration definition '%s' must be ".
+					"of the form section.name.", $qualified_name));
+			}
+
+			list($name, $section) = explode('.', $qualified_name, 2);
+			$this->addDefinition($section, $name, $default_value);
+		}
+	}
+
+	// }}}
+	// {{{ public function addDefinition()
+
+	/**
+	 * Adds a configuration setting definition to this config module
+	 *
+	 * Only defined settings may be loaded from an ini file. Other settings
+	 * will throw exceptions when they are loaded.
+	 *
+	 * @param string $section the section the setting belongs to.
+	 * @param string $name the name of the setting.
+	 * @param string $default_value optional. The default value of the setting.
+	 *                               Used if the setting does exist in the
+	 *                               loaded ini file.
+	 *
+	 * @throws SiteException if the section is not a valid section name. Valid
+	 *                       section names follow the rules for PHP identifiers.
+	 * @throws SiteException if the name is not a valid name. Valid names follow
+	 *                       the rules for PHP identifiers.
+	 */
+	public function addDefinition($section, $name, $default_value = null)
+	{
+		$section = (string)$section;
+		$name = (string)$name;
+
+		if (!$this->isValidKey($section)) {
+			throw new SiteException(sprintf(
+				"Section '%s' in configuration definition is not a valid name.",
+				$section));
+		}
+
+		if (!$this->isValidKey($name)) {
+			throw new SiteException(sprintf(
+				"Name '%s' in configuration definition is not a valid name.",
+				$name));
+		}
+
+		if (!array_key_exists($section, $this->definitions))
+			$this->definitions[$section] = array();
+
+		if (!array_key_exists($name, $this->definitions[$section])
+			$this->definitions[$section] = array();
+
+		$this->definitions[$section][$name] = $default_value;
+	}
+
+	// }}}
+	// {{{ private function isValidKey()
+
+	/**
+	 * Checks whether or not a name is a valid section name or setting name
+	 *
+	 * @param string $name the name to check.
+	 *
+	 * @return boolean true if the name is valid and false if it is not.
+	 */
+	private function isValidKey($name)
 	{
 		$regexp = '/^[a-zA-Z_\\x7f-\\xff][a-zA-Z0-9_\\x7f-\\xff]*$/';
 		return (preg_match($regexp, $name) == 1);
