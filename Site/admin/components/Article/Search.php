6ca2ca1e686759ca28f3d30b0628f4b5fd19cbde
Michael Gauthier <mike@silverorange.com>
2007-11-27 16:00:06 +0000

    Article part of Ticket #1396: Update all admin searches to use new NateGoSearch methd.
    svn commit r23555

--- Site/admin/components/Article/Search.php
+++ Site/admin/components/Article/Search.php
@@ -3,7 +3,8 @@
 require_once 'Admin/pages/AdminSearch.php';
 require_once 'Admin/AdminSearchClause.php';
 require_once 'SwatDB/SwatDB.php';
-require_once 'NateGoSearch/NateGoSearchQuery.php';
+require_once 'NateGoSearch/NateGoSearch.php';
+require_once 'Site/SiteNateGoFulltextSearchEngine.php';
 
 require_once 'include/SiteArticleActionsProcessor.php';
 require_once 'include/SiteArticleVisibilityCellRenderer.php';
@@ -19,8 +20,13 @@ class SiteArticleSearch extends AdminSearch
 {
 	// {{{ protected properties
 
-	protected $where_clause;
+	/**
+	 * @var NateGoFulltextSearchResult
+	 */
+	protected $fulltext_result;
+
 	protected $join_clause;
+	protected $where_clause;
 	protected $order_by_clause;
 
 	/**
@@ -84,6 +90,103 @@ class SiteArticleSearch extends AdminSearch
 	}
 
 	// }}}
+	// {{{ protected function getTableModel()
+
+	protected function getTableModel(SwatView $view)
+	{
+		$this->searchArticles();
+
+		$sql = sprintf('select count(id) from Article %s where %s',
+			$this->getJoinClause(),
+			$this->getWhereClause());
+
+		$pager = $this->ui->getWidget('pager');
+		$pager->total_records = SwatDB::queryOne($this->app->db, $sql);
+
+		$sql = 'select Article.id,
+					Article.title,
+					Article.show,
+					Article.searchable
+				from Article
+				%s
+				where %s
+				order by %s';
+
+		$sql = sprintf($sql,
+			$this->getJoinClause(),
+			$this->getWhereClause(),
+			$this->getOrderByClause($view, ''));
+
+		$this->app->db->setLimit($pager->page_size, $pager->current_record);
+		$rs = SwatDB::query($this->app->db, $sql);
+
+		$this->ui->getWidget('results_frame')->visible = true;
+		$view = $this->ui->getWidget('index_view');
+		$view->getColumn('visibility')->getRendererByPosition()->db =
+			$this->app->db;
+
+		if (count($rs) > 0) {
+			$this->ui->getWidget('results_message')->content =
+				$pager->getResultsMessage(Site::_('result'),
+					Site::_('results'));
+		}
+
+		return $rs;
+	}
+
+	// }}}
+	// {{{ protected function searchArticles()
+
+	protected function searchArticles()
+	{
+		$keywords = $this->ui->getWidget('search_keywords')->value;
+		if (strlen(trim($keywords)) == 0 || $this->getSearchType() === null) {
+			$this->fulltext_result = null;
+		} else {
+			$fulltext_engine = new SiteNateGoFulltextSearchEngine(
+				$this->app->db);
+
+			$fulltext_engine->setTypes(array(
+				$this->getSearchType(),
+			));
+
+			$this->fulltext_result = $fulltext_engine->search($keywords);
+		}
+	}
+
+	// }}}
+	// {{{ protected function getSearchType()
+
+	/**
+	 * Gets the search type for articles for this web-application
+	 *
+	 * @return integer the search type for articles for this web-application or
+	 *                  null if fulltext searching is not implemented for the
+	 *                  current application.
+	 */
+	protected function getSearchType()
+	{
+		return 'article';
+	}
+
+	// }}}
+	// {{{ protected function getJoinClause()
+
+	protected function getJoinClause()
+	{
+		if ($this->join_clause === null) {
+			if ($this->fulltext_result === null) {
+				$this->join_clause = '';
+			} else {
+				$this->join_clause = $this->fulltext_result->getJoinClause(
+					'id', $this->getSearchType());
+			}
+		}
+
+		return $this->join_clause;
+	}
+
+	// }}}
 	// {{{ protected function getWhereClause()
 
 	protected function getWhereClause()
@@ -93,8 +196,9 @@ class SiteArticleSearch extends AdminSearch
 
 			// keywords are included in the where clause if fulltext searching
 			// is turned off
-			if ($this->getArticleSearchType() === null &&
-				$this->ui->getWidget('search_keywords')->value != null) {
+			$keywords = $this->ui->getWidget('search_keywords')->value;
+			if (strlen(trim($keywords)) > 0 &&
+				$this->getSearchType() === null) {
 
 				$where.= ' and ( ';
 
@@ -114,115 +218,42 @@ class SiteArticleSearch extends AdminSearch
 			}
 
 			$clause = new AdminSearchClause('boolean:show');
-			$clause->value = 
+			$clause->value =
 				$this->ui->getWidget('search_show')->value;
 
 			$where.= $clause->getClause($this->app->db);
 
 			$clause = new AdminSearchClause('boolean:searchable');
-			$clause->value = 
+			$clause->value =
 				$this->ui->getWidget('search_searchable')->value;
 
 			$where.= $clause->getClause($this->app->db);
 
-			$this->where = $where;
+			$this->where_clause = $where;
 		}
 
-		return $this->where;
+		return $this->where_clause;
 	}
 
 	// }}}
-	// {{{ protected function getTableModel()
+	// {{{ protected function getOrderByClause()
 
-	protected function getTableModel(SwatView $view)
+	protected function getOrderByClause($view, $default_orderby)
 	{
-		$this->searchArticles();
-
-		$sql = sprintf('select count(id) from Article %s where %s',
-			$this->join_clause,
-			$this->getWhereClause());
-
-		$pager = $this->ui->getWidget('pager');
-		$pager->total_records = SwatDB::queryOne($this->app->db, $sql);
-
-		$sql = 'select Article.id,
-					Article.title,
-					Article.show,
-					Article.searchable
-				from Article
-				%s
-				where %s
-				order by %s';
-
-		$sql = sprintf($sql,
-			$this->join_clause,
-			$this->getWhereClause(),
-			$this->getOrderByClause($view, $this->order_by_clause));
-
-		$this->app->db->setLimit($pager->page_size, $pager->current_record);
-		$rs = SwatDB::query($this->app->db, $sql);
-
-		$this->ui->getWidget('results_frame')->visible = true;
-		$view = $this->ui->getWidget('index_view');
-		$view->getColumn('visibility')->getRendererByPosition()->db =
-			$this->app->db;
-
-		if (count($rs) > 0)
-			$this->ui->getWidget('results_message')->content =
-				$pager->getResultsMessage(Site::_('result'),
-					Site::_('results'));
-
-		return $rs;
-	}
-
-	// }}}
-	// {{{ protected function searchArticles()
-
-	protected function searchArticles()
-	{
-		$keywords = $this->ui->getWidget('search_keywords')->value;
-		if (strlen(trim($keywords)) > 0 &&
-			$this->getArticleSearchType() !== null) {
-
-			$query = new NateGoSearchQuery($this->app->db);
-			$query->addDocumentType($this->getArticleSearchType());
-			$query->addBlockedWords(
-				NateGoSearchQuery::getDefaultBlockedWords());
-
-			$result = $query->query($keywords);
-
-			$this->join_clause = sprintf(
-				'inner join %1$s on
-					%1$s.document_id = Article.id and
-					%1$s.unique_id = %2$s and %1$s.document_type = %3$s',
-				$result->getResultTable(),
-				$this->app->db->quote($result->getUniqueId(), 'text'),
-				$this->app->db->quote($this->getArticleSearchType(),
-					'integer'));
+		if ($this->order_by_clause === null) {
+			if ($this->fulltext_result === null) {
+				$order_by_clause = 'Article.title';
+			} else {
+				// AdminSearch expects no 'order by' in returned value.
+				$order_by_clause = str_replace('order by ', '',
+					$this->fulltext_result->getOrderByClause('Article.title'));
+			}
 
 			$this->order_by_clause =
-				sprintf('%1$s.displayorder1, %1$s.displayorder2, Article.title',
-					$result->getResultTable());
-		} else {
-			$this->join_clause = '';
-			$this->order_by_clause = 'Article.title';
+				parent::getOrderByClause($view, $order_by_clause);
 		}
-	}
 
-	// }}}
-	// {{{ protected function getArticleSearchType()
-
-	/**
-	 * Gets the search type for articles for this web-application
-	 *
-	 * @return integer the search type for articles for this web-application or
-	 *                  null if fulltext searching is not implemented for the
-	 *                  current application.
-	 */
-	protected function getArticleSearchType()
-	{
-		return null;
-		//return NateGoSearch::getDocumentType($this->app->db, 'article');
+		return $this->order_by_clause;
 	}
 
 	// }}}
