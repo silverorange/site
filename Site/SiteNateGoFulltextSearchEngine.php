5cb396ce1b2c8b9e71c2bc1b1a68d4e6f03bf2c8
Charles Waddell <charles@silverorange.com>
2007-11-30 20:26:45 +0000

    Part of ticket #1548, the indexer will now build a custom wordlist for the site and then use that list for spell checking search terms.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@23696 94490950-a86c-4493-9ccb-530f72933e47

--- Site/SiteNateGoFulltextSearchEngine.php
+++ Site/SiteNateGoFulltextSearchEngine.php
@@ -83,6 +83,9 @@ class SiteNateGoFulltextSearchEngine extends SwatObject
 	public function search($keywords)
 	{
 		$spell_checker = new NateGoSearchPSpellSpellChecker($this->locale);
+		$spell_checker->setCustomWordList($this->getCustomWordList());
+		$spell_checker->loadCustomContent();
+
 		$query = new NateGoSearchQuery($this->db);
 		$query->addBlockedWords(NateGoSearchQuery::getDefaultBlockedWords());
 		$query->setSpellChecker($spell_checker);
@@ -93,6 +96,21 @@ class SiteNateGoFulltextSearchEngine extends SwatObject
 		$result = $query->query($keywords);
 
 		return new SiteNateGoFulltextSearchResult($this->db, $result);
+	}
+
+	// }}}
+	// {{{ protected function getCustomWordList()
+
+	/**
+	 * Get the custom word list
+	 *
+	 * Get the custom word list that is used by this fulltext search engine.
+	 *
+	 * @return string the path to the custom word list
+	 */
+	protected function getCustomWordList()
+	{
+		return './../system/search/custom-wordlist.pws';
 	}
 
 	// }}}
