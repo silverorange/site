56746654e7dabf070ba44ca508ec305f21bc2cb0
Michael Gauthier <mike@silverorange.com>
2007-12-10 18:03:44 +0000

    Ad dataobjects for site package.
    svn commit r24185

--- Site/dataobjects/SiteAd.php
+++ Site/dataobjects/SiteAd.php
@@ -3,17 +3,17 @@
 require_once 'SwatDB/SwatDBDataObject.php';
 
 /**
- * Advertisements that are tracked on an e-commerce web applcation
+ * Advertisements that are tracked on an applcation
  *
  * This class is for tracking advertisements, not for displaying
  * advertisements.
  *
- * @package   Store
+ * @package   Site
  * @copyright 2006-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
- * @see       StoreAdReferer
+ * @see       SiteAdReferrer
  */
-class StoreAd extends SwatDBDataObject
+class SiteAd extends SwatDBDataObject
 {
 	// {{{ public properties
 
@@ -67,6 +67,39 @@ class StoreAd extends SwatDBDataObject
 		$this->table = 'Ad';
 		$this->id_field = 'integer:id';
 		$this->registerDateProperty('createdate');
+	}
+
+	// }}}
+	// {{{ public function loadFromShortname()
+
+	/**
+	 * Loads an ad from the database using the ad shortname
+	 *
+	 * @param string $shortname the shortname of the ad.
+	 *
+	 * @return boolean true if the loading was successful and false if it was
+	 *                  not.
+	 */
+	public function loadFromShortname($shortname)
+	{
+		$this->checkDB();
+		$row = null;
+
+		if ($this->table !== null) {
+			$sql = sprintf('select * from %s where shortname = %s',
+				$this->table,
+				$this->db->quote($shortname, 'text'));
+
+			$rs = SwatDB::query($this->db, $sql, null);
+			$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
+		}
+
+		if ($row === null)
+			return false;
+
+		$this->initFromRow($row);
+		$this->generatePropertyHashes($row);
+		return true;
 	}
 
 	// }}}
