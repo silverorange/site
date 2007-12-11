13e2459aa69261f8307844e3cd3690223b43fb3e
Michael Gauthier <mike@silverorange.com>
2007-12-11 18:21:28 +0000

    Set title on autogenerated ad object.
    svn commit r24326

--- Site/SiteAnalyticsModule.php
+++ Site/SiteAnalyticsModule.php
@@ -296,6 +296,7 @@ class SiteAnalyticsModule extends SiteApplicationModule
 		$class_name = SwatDBClassMap::get('SiteAd');
 		$ad = new $class_name();
 
+		$ad->title = sprintf(Site::_('Ad %s'), (string)$shortname);
 		$ad->shortname = (string)$shortname;
 		$ad->createdate = new SwatDate();
 		$ad->createdate->toUTC();
