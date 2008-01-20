24dccb8fdde5e3ceafd36012c044e9db31f8fef3
Nick Burka <nick@silverorange.com>
2008-01-20 02:11:49 +0000

    Changes and additions to the image tables
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@26148 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/ImageDimensionBinding.sql
+++ sql/tables/ImageDimensionBinding.sql
@@ -1,6 +1,7 @@
 create table ImageDimensionBinding (
 	image integer not null references Image(id),
 	dimension integer not null references ImageDimension(id),
+	image_type integer not null references ImageType(id),
 	width integer not null,
 	height integer not null,
 	primary key(image, dimension)
---
+++ sql/tables/ImageType.sql
@@ -0,0 +1,6 @@
+create table ImageType (
+	id serial,
+	extension varchar(10),
+	mime_type varchar(50),
+	primary key(id)
+);
