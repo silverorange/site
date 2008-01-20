aa131acf7e3f5100e9b66fbbe9fb8fa8a435e04e
Nick Burka <nick@silverorange.com>
2008-01-20 19:30:51 +0000

    Smart handling of deleting images. Image files are automatically deleted if the dataobject is deleted.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@26154 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/ImageDimensionBinding.sql
+++ sql/tables/ImageDimensionBinding.sql
@@ -1,6 +1,6 @@
 create table ImageDimensionBinding (
-	image integer not null references Image(id),
-	dimension integer not null references ImageDimension(id),
+	image integer not null references Image(id) on delete cascade,
+	dimension integer not null references ImageDimension(id) on delete cascade,
 	image_type integer not null references ImageType(id),
 	width integer not null,
 	height integer not null,
