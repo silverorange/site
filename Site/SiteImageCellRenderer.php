57c24a219b2079fb3ac0c4ecd6a6bb9af183e0a1
Nick Burka <nick@silverorange.com>
2008-01-22 18:52:08 +0000

    New site image cell renderer for handling SiteImage dataobjects nicely.
    git-svn-id: file:///Users/gauthierm/svnrepo/site/trunk@26316 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Site/SiteImageCellRenderer.php
@@ -0,0 +1,151 @@
+<?php
+
+require_once 'Swat/SwatHtmlTag.php';
+require_once 'Swat/SwatCellRenderer.php';
+require_once 'Swat/SwatImageCellRenderer.php';
+
+/**
+ * A cell renderer for SiteImages
+ *
+ * @package   Site
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteImageCellRenderer extends SwatCellRenderer
+{
+	// {{{ class constants
+
+	/**
+	 * Title length before it gets truncated.
+	 */
+	const MAX_TITLE_LENGTH = 30;
+
+	// }}}
+	// {{{ public properties
+
+	/**
+	 * @var SiteImage
+	 */
+	public $image;
+
+	/**
+	 * @var string
+	 */
+	public $image_dimension;
+
+	/**
+	 * @var string
+	 */
+	public $path_prefix = null;
+
+	/**
+	 * The href attribute in the XHTML anchor tag
+	 *
+	 * Optionally uses vsprintf() syntax, for example:
+	 * <code>
+	 * $renderer->link = 'MySection/MyPage/%s?id=%s';
+	 * </code>
+	 *
+	 * @var string
+	 */
+	public $link;
+
+	/**
+	 * A value or array of values to substitute into the link of this cell. The
+	 * value will automatically be url encoded when it is included in the link.
+	 *
+	 * @var mixed
+	 */
+	public $link_value = null;
+
+	// }}}
+	// {{{ private properties
+
+	/**
+	 * @var SwatImageCellRenderer
+	 */
+	private $image_cell_renderer;
+
+	// }}}
+	// {{{ public function __construct()
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->image_cell_renderer = new SwatImageCellRenderer();
+		$this->image_cell_renderer->parent = $this;
+	}
+
+	// }}}
+	// {{{ public function getHtmlHeadEntrySet()
+
+	public function getHtmlHeadEntrySet()
+	{
+		$set = parent::getHtmlHeadEntrySet();
+		$set->addEntrySet($this->image_cell_renderer->getHtmlHeadEntrySet());
+		return $set;
+	}
+
+	// }}}
+	// {{{ public function render()
+
+	public function render()
+	{
+		if (!$this->visible)
+			return;
+
+		if ($this->image->title === null)
+			$title = null;
+		else
+			$title = SwatString::condense($this->image->title,
+				self::MAX_TITLE_LENGTH);
+
+		if ($this->link !== null) {
+			$a_tag = new SwatHtmlTag('a');
+
+			if ($this->link_value === null)
+				$a_tag->href = $this->link;
+			elseif (is_array($this->link_value))
+				$a_tag->href = vsprintf($this->link, $this->link_value);
+			else
+				$a_tag->href = sprintf($this->link, $this->link_value);
+
+			$a_tag->open();
+		}
+
+		$this->image_cell_renderer->image =
+			$this->image->getUri($this->image_dimension, $this->path_prefix);
+		$this->image_cell_renderer->width =
+			$this->image->getWidth($this->image_dimension);
+		$this->image_cell_renderer->height =
+			$this->image->getHeight($this->image_dimension);
+		$this->image_cell_renderer->alt = '';
+
+		$image_wrapper_tag = new SwatHtmlTag('span');
+		$image_wrapper_tag->class = 'site-image-wrapper';
+
+		$image_wrapper_tag->open();
+		$this->image_cell_renderer->render();
+		$image_wrapper_tag->close();
+
+		$span_tag = new SwatHtmlTag('span');
+		$span_tag->class = 'title';
+		if ($title === null)
+			$span_tag->setContent(''); // prevent self-closing span tag
+		else
+			$span_tag->setContent($title);
+
+		if (strlen($this->image->title) > self::MAX_TITLE_LENGTH)
+			$span_tag->title = $this->image->title;
+
+		$span_tag->display();
+
+		if ($this->link !== null)
+			$a_tag->close();
+	}
+
+	// }}}
+}
+
+?>
