2fb0e90367c11f0d610719525701fd16e15de02f
Michael Gauthier <mike@silverorange.com>
2008-01-28 22:40:48 +0000

    Push constructor up to article path.
    svn commit r26724

--- Site/SiteArticlePath.php
+++ Site/SiteArticlePath.php
@@ -8,6 +8,23 @@ require_once 'Site/SitePath.php';
  */
 class SiteArticlePath extends SitePath
 {
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new article path object
+	 *
+	 * @param SiteWebApplication $app the application this path exists in.
+	 * @param integer $id the database id of the object to create the path for.
+	 *                     If no database id is specified, an empty path is
+	 *                     created.
+	 */
+	public function __construct(SiteWebApplication $app, $id = null)
+	{
+		if ($id !== null)
+			$this->loadFromId($app, $id);
+	}
+
+	// }}}
 	// {{{ protected function loadFromId()
 
 	/**
--- Site/SitePath.php
+++ Site/SitePath.php
@@ -33,23 +33,6 @@ class SitePath implements Iterator, Countable
 	private $current_index = 0;
 
 	// }}}
-	// {{{ public function __construct()
-
-	/**
-	 * Creates a new path object
-	 *
-	 * @param SiteWebApplication $app the application this path exists in.
-	 * @param integer $id the database id of the object to create the path for.
-	 *                     If no database id is specified, an empty path is
-	 *                     created.
-	 */
-	public function __construct(SiteWebApplication $app, $id = null)
-	{
-		if ($id !== null)
-			$this->loadFromId($app, $id);
-	}
-
-	// }}}
 	// {{{ public final function addEntry()
 
 	/**
