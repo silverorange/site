21c9263b416d81874ed0e32849de4c5f6ab18c9b
Charles Waddell <charles@silverorange.com>
2008-02-01 15:52:39 +0000

    Fixes ticket #1967, For whatever odd reason comparing the instances was returning false. Comparing the instance ids works much better.
    svn commit r26955

--- Site/admin/components/Account/Details.php
+++ Site/admin/components/Account/Details.php
@@ -64,10 +64,11 @@ class SiteAccountDetails extends AdminIndex
 					Site::_('An account with an id of ‘%d’ does not exist.'),
 					$this->id));
 			elseif ($this->app->hasModule('SiteMultipleInstanceModule') &&
-				$this->account->instance != $this->app->instance->getInstance())
-				throw new AdminNotFoundException(sprintf(
-					Store::_('Incorrect instance for account ‘%d’.'),
-						$this->id));
+				$this->account->instance->id !=
+				$this->app->instance->getInstance()->id)
+					throw new AdminNotFoundException(sprintf(
+						Store::_('Incorrect instance for account ‘%d’.'),
+							$this->id));
 		}
 
 		return $this->account;
