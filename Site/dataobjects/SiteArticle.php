2c39ea0621d0921119e661bcf0ce4bd65deef9b9
Charles Waddell <charles@silverorange.com>
2008-02-13 17:34:27 +0000

    Give site articles the abilty to be loaded from a shortname.
    svn commit r27633

--- Site/dataobjects/SiteArticle.php
+++ Site/dataobjects/SiteArticle.php
@@ -235,6 +235,42 @@ class SiteArticle extends SwatDBDataObject
 	}
 
 	// }}}
+	// {{{ public function loadFromShortname()
+
+	/**
+	 * Loads an article from its shortname
+	 *
+	 * @param string $shortname the shortname of the article to load.
+	 *
+	 * @return boolean true if the loading of this article was successfil and
+	 *                  false if the article with the given shortname doesn't
+	 *                  exist.
+	 */
+	public function loadFromShortname($shortname)
+	{
+		$this->checkDB();
+
+		$row = null;
+
+		if ($this->table !== null) {
+			$sql = sprintf('select * from %s where shortname = %s',
+				$this->table,
+				$this->db->quote($shortname));
+
+			$rs = SwatDB::query($this->db, $sql, null);
+			$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
+		}
+
+		if ($row === null)
+			return false;
+
+		$this->initFromRow($row);
+		$this->generatePropertyHashes();
+
+		return true;
+	}
+	
+	// }}}
 	// {{{ protected function init()
 
 	protected function init()
