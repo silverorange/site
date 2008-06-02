70f5105d347edc2029e6c1233009625cae125352
Michael Gauthier <mike@silverorange.com>
2008-06-02 21:05:35 +0000

    Add basic theme module to Site.
    svn commit r33119

---
+++ Site/SiteThemeModule.php
@@ -0,0 +1,232 @@
+<?php
+
+require_once 'Site/SiteApplicationModule.php';
+
+/**
+ * Web application module for handling themeing
+ *
+ * @package   Site
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteThemeModule extends SiteApplicationModule
+{
+	// {{{ protected properties
+
+	/**
+	 * List of paths to search for themes
+	 *
+	 * By defualt, the relative path '../themes' exists.
+	 *
+	 * @var array
+	 *
+	 * @see SiteThemeModule::addPath()
+	 */
+	protected $theme_paths = array('../themes');
+
+	/**
+	 * The current theme
+	 *
+	 * Null if there is no current theme.
+	 *
+	 * @var string
+	 */
+	protected $theme;
+
+	// }}}
+	// {{{ public function init()
+
+	public function init()
+	{
+	}
+
+	// }}}
+	// {{{ public function set()
+
+	/**
+	 * Sets the theme to use
+	 *
+	 * @param string $theme the shortname of the theme to use. Theme names may
+	 *                       contain letters, numbers, underscores, dashes and
+	 *                       periods.
+	 *
+	 * @throws InvalidArgumentException if the theme name is not a valid theme
+	 *                                  name.
+	 */
+	public function set($theme)
+	{
+		if (preg_match('/^[\d\w-\.]+$/u', $theme) === 0) {
+			throw new InvalidArgumentException(sprintf('Theme name "%s" is '.
+				'not a valid theme name.'));
+		}
+
+		$this->theme = $theme;
+	}
+
+	// }}}
+	// {{{ public function addPath()
+
+	/**
+	 * Adds a theme path to this module
+	 *
+	 * All theme paths are relative to the PHP include path. The relative path
+	 * '../themes' is included by default.
+	 *
+	 * @param string $path the theme path to add.
+	 *
+	 * @see SiteThemeModule::removePath()
+	 */
+	public function addPath($path)
+	{
+		if (!in_array($path, $this->theme_paths, true)) {
+			// add path to front of array since it is more likely we will find
+			// theme files in manually added paths
+			array_unshift($this->theme_paths, $path);
+		}
+	}
+
+	// }}}
+	// {{{ public function removePath()
+
+	/**
+	 * Removes a theme path from this module
+	 *
+	 * @param string $path the path to remove.
+	 *
+	 * @see SiteThemeModule::addPath()
+	 */
+	public function removePath($path)
+	{
+		$index = array_search($path, $this->theme_paths);
+		if ($index !== false) {
+			array_splice($this->theme_paths, $index, 1);
+		}
+	}
+
+	// }}}
+	// {{{ public function getLayoutClass()
+
+	public function getLayoutClass($base_layout_class)
+	{
+		$class_name = $this->getCamelCaseShortname().'Layout';
+		$file_name  = $this->getFile('layouts/'.$class_name.'.php');
+
+		if ($file_name === null) {
+			$class_name = $base_layout_class;
+		} else {
+			require_once $file_name;
+
+			if (!class_exists($class_name)) {
+				throw new SiteException(sprintf('Layout file "%s" must '.
+					'contain a class named "%s"',
+					$file_name, $class_name));
+			}
+
+			if (!is_subclass_of($class_name, $base_layout_class)) {
+				throw new SiteException(sprintf('Layout class "%s" must '.
+					'be a subclass of %s.',
+					$class_name, $base_layout_class));
+			}
+		}
+
+		return $class_name;
+	}
+
+	// }}}
+	// {{{ public function getTemplateFile()
+
+	public function getTemplateFile($default_template)
+	{
+		$file_name = $this->getFile('layouts/xhtml/template.php');
+
+		if ($file_name === null) {
+			$file_name = $default_template;
+		}
+
+		return $file_name;
+	}
+
+	// }}}
+	// {{{ public function getCssFile()
+
+	public function getCssFile()
+	{
+		$file = $this->getFile('www/styles/theme.css');
+
+		if ($file !== null) {
+			// get www accessible version
+			$file = 'themes/'.$this->theme.'/styles/theme.css';
+		}
+
+		return $file;
+	}
+
+	// }}}
+	// {{{ protected function getFile()
+
+	/**
+	 * Checks whether or not a file exists for the current theme
+	 *
+	 * This checks all the theme paths relative to the include path.
+	 *
+	 * @return string the full filename if the file exists for the current
+	 *                theme and null if no such file exists for the current
+	 *                theme.
+	 */
+	protected function getFile($filename)
+	{
+		$return_filename = null;
+		$include_paths   = explode(PATH_SEPARATOR, get_include_path());
+
+		foreach ($this->theme_paths as $theme_path) {
+			// normalize Windows paths to UNIX format
+			$theme_path = end(explode(':', $theme_path, 2));
+			$theme_path = str_replace('\\', '/', $theme_path);
+
+			// check if path is absolute or relative to the include path
+			if ($theme_path[0] == '/') {
+				$temp_filename = $theme_path.'/'.$this->theme.'/'.$filename;
+				if (file_exists($temp_filename)) {
+					$return_filename = $temp_filename;
+					break;
+				}
+			} else {
+				foreach ($include_paths as $include_path) {
+					$temp_filename = $include_path.'/'.$theme_path.'/'.
+						$this->theme.'/'.$filename;
+
+					if (file_exists($temp_filename)) {
+						$return_filename = $temp_filename;
+						break 2;
+					}
+				}
+			}
+		}
+
+		return $return_filename;
+	}
+
+	// }}}
+	// {{{ protected function getCamelCaseShortname()
+
+	/**
+	 * Gets the camel-case variant of the theme shortname
+	 *
+	 * @return string the camel-case variant of the theme shortname.
+	 */
+	protected function getCamelCaseShortname()
+	{
+		$theme = $this->theme;
+
+		$theme = str_replace('_', ' ', $theme);
+		$theme = str_replace('.', ' ', $theme);
+		$theme = str_replace('-', ' ', $theme);
+
+		return str_replace(' ', '', ucwords($theme));
+	}
+
+	// }}}
+}
+
+?>
+
