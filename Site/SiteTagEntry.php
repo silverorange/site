93bd0bbc9be938602058c49c1f0e85365713338d
Nick Burka <nick@silverorange.com>
2008-06-04 18:58:35 +0000

    A tag entry widget ported from Pinhole.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@33229 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Site/SiteTagEntry.php
@@ -0,0 +1,289 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/exceptions/SwatException.php';
+require_once 'Swat/exceptions/SwatInvalidClassException.php';
+require_once 'Swat/SwatInputControl.php';
+require_once 'Swat/SwatHtmlTag.php';
+require_once 'Swat/SwatState.php';
+require_once 'Swat/SwatString.php';
+require_once 'Swat/SwatMessage.php';
+require_once 'Swat/SwatYUI.php';
+
+/**
+ * Control for creating new tags and selecting multiple tags from a array of tags
+ *
+ * @package   Site
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+abstract class SiteTagEntry extends SwatInputControl implements SwatState
+{
+	// {{{ protected properties
+
+	/**
+	 * An associative array of tags for the tag flydown in the form of
+	 * name => title
+	 *
+	 * @var array
+	 */
+	protected $tag_array;
+
+	/**
+	 * An array of tag names selected by this tag entry control
+	 *
+	 * @var array
+	 */
+	protected $selected_tag_array;
+
+	// }}}
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new tag entry control
+	 *
+	 * @param string $id a non-visible unique id for this widget.
+	 *
+	 * @see SwatWidget::__construct()
+	 */
+	public function __construct($id = null)
+	{
+		parent::__construct($id);
+
+		$this->requires_id = true;
+
+		$yui = new SwatYUI(array('autocomplete'));
+		$this->html_head_entry_set->addEntrySet($yui->getHtmlHeadEntrySet());
+
+		$this->addJavaScript('packages/site/javascript/site-tag-entry.js',
+			Site::PACKAGE_ID);
+
+		$this->addStyleSheet('packages/site/styles/site-tag-entry.css',
+			Site::PACKAGE_ID);
+	}
+
+	// }}}
+	// {{{ public function display()
+
+	/**
+	 * Displays this tag entry control
+	 */
+	public function display()
+	{
+		if (!$this->visible)
+			return;
+
+		SwatWidget::display();
+
+		$div_tag = new SwatHtmlTag('div');
+		$div_tag->class = 'site-tag-entry';
+		$div_tag->id = $this->id;
+		$div_tag->open();
+
+		$input_tag = new SwatHtmlTag('input');
+		$input_tag->name = $this->id.'_value';
+		$input_tag->id = $this->id.'_value';
+
+		if (!$this->isSensitive())
+			$input_tag->disabled = 'disabled';
+
+		$input_tag->display();
+
+		$container_tag = new SwatHtmlTag('div');
+		$container_tag->class = 'site-tag-container';
+		$container_tag->id = $this->id.'_container';
+		$container_tag->setContent('');
+		$container_tag->display();
+
+		$ul_tag = new SwatHtmlTag('ul');
+		$ul_tag->id = $this->id.'_array';
+		$ul_tag->class = 'site-tag-array';
+		$ul_tag->open();
+		// Array left blank. Values are filled in via javascript.
+		$ul_tag->close();
+
+		$div_tag->close();
+
+		Swat::displayInlineJavaScript($this->getInlineJavaScript());
+	}
+
+	// }}}
+	// {{{ public function process()
+
+	/**
+	 * Processes this tag entry control
+	 *
+	 * If a validation error occurs, an error message is attached to this
+	 * widget.
+	 *
+	 * @throws SwatException if no database connection is set on this tag
+	 *                        entry control.
+	 */
+	public function process()
+	{
+		parent::process();
+
+		$this->selected_tag_array = array();
+
+		$data = &$this->getForm()->getFormData();
+		$new_key = $this->id.'_new';
+		if (isset($data[$new_key]) && is_array($data[$new_key]))
+			foreach ($data[$new_key] as $new_tag)
+				$this->insertTag($new_tag);
+
+		if (isset($data[$this->id]) && is_array($data[$this->id])) {
+			$tag_strings = $data[$this->id];
+
+			// make sure entered tags are in the original tag array
+			foreach ($tag_strings as $tag_string)
+				if (array_key_exists($tag_string, $this->tag_array))
+					$this->selected_tag_array[] = $tag_string;
+		}
+
+		if ($this->required && count($this->selected_tag_array) == 0) {
+			$message = Swat::_('The %s field is required.');
+			$this->addMessage(new SwatMessage($message, SwatMessage::ERROR));
+		}
+	}
+
+	// }}}
+	// {{{ public function getState()
+
+	/**
+	 * Gets the current state of this tag entry control
+	 *
+	 * @return boolean the current state of this tag entry control.
+	 *
+	 * @see SwatState::getState()
+	 */
+	public function getState()
+	{
+		return $this->getSelectedTagArray();
+	}
+
+	// }}}
+	// {{{ public function setState()
+
+	/**
+	 * Sets the current state of this tag entry control
+	 *
+	 * @param array $state the new state of this tag entry control.
+	 *
+	 * @see SwatState::setState()
+	 *
+	 * @throws SwatException if the given state is not an array
+	 */
+	public function setState($state)
+	{
+		if (is_array($state))
+			$this->selected_tag_array = $state;
+		else
+			throw new SwatException('State must be an array');
+	}
+
+	// }}}
+	// {{{ public function getFocusableHtmlId()
+
+	/**
+	 * Gets the id attribute of the XHTML element displayed by this widget
+	 * that should receive focus
+	 *
+	 * @return string the id attribute of the XHTML element displayed by this
+	 *                 widget that should receive focus or null if there is
+	 *                 no such element.
+	 *
+	 * @see SwatWidget::getFocusableHtmlId()
+	 */
+	public function getFocusableHtmlId()
+	{
+		return ($this->visible) ? $this->id.'_value' : null;
+	}
+
+	// }}}
+	// {{{ public function setTagArray()
+
+	/**
+	 * Sets the array of tags that may be selected by this tag entry control
+	 *
+	 * @param array $tag_array the array of tags that may be selected by
+	 *                                  this tag entry control.
+	 */
+	public function setTagArray(array $tag_array)
+	{
+		$this->tag_array = $tag_array;
+	}
+
+	// }}}
+	// {{{ public function setSelectedTagArray()
+
+	/**
+	 * Sets the array of tags that are pre-selected for this photo
+	 *
+	 * @param array $tag_array the array of tags that appear
+	 *                       pre-selected for this entry widget.
+ 	 */
+	public function setSelectedTagArray(array $tag_array)
+	{
+		$this->selected_tag_array = $tag_array;
+	}
+
+	// }}}
+	// {{{ public function getSelectedTagArray()
+
+	/**
+	 * Gets the array of tags selected by this tag entry control
+	 *
+	 * Call this method after processing this control to get the tags selected
+	 * by the user.
+	 *
+	 * @return array the array of tags selected by this tag entry
+	 *                         control.
+	 */
+	public function getSelectedTagArray()
+	{
+		return $this->selected_tag_array;
+	}
+
+	// }}}
+	// {{{ protected function getInlineJavaScript()
+
+	/**
+	 * Gets the inline JavaScript for this tag entry control
+	 *
+	 * @return string the inline JavaScript for this tag entry control.
+	 */
+	protected function getInlineJavaScript()
+	{
+		$tag_array = array();
+		foreach ($this->tag_array as $tag => $title)
+			$tag_array[] = sprintf("\n[%s, %s]",
+				SwatString::quoteJavaScriptString($title),
+				SwatString::quoteJavaScriptString($tag));
+
+		$selected_array = array();
+		if ($this->selected_tag_array !== null) {
+			foreach ($this->selected_tag_array as $tag)
+				$selected_array[] =
+					SwatString::quoteJavaScriptString($tag);
+		}
+
+		return sprintf("var %1\$s_obj = new SiteTagEntry(".
+			"'%1\$s', [%2\$s], [%3\$s]);",
+			$this->id,
+			implode(',', $tag_array),
+			implode(',', $selected_array));
+	}
+
+	// }}}
+	// {{{ abstract protected function insertTag()
+
+	/**
+	 * Creates a new tag
+	 */
+	abstract protected function insertTag($title);
+
+	// }}}
+}
+
+?>
---
+++ www/javascript/site-tag-entry.js
@@ -0,0 +1,261 @@
+/**
+ * Control for selecting multiple tags from a array of tags
+ *
+ * @package   Site
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+
+// {{{ SiteTagEntry()
+
+/**
+ * Creates a new tag entry widget
+ *
+ * @param string id the unique identifier of this entry object.
+ * @param array tag_array an array of tag strings and titles that are possible
+ *                        to select using this control
+ * @param array initial_selected_tag_array an array of already selected tag
+ *                                         strings.
+ */
+function SiteTagEntry(id, tag_array, initial_selected_tag_array)
+{
+	this.id = id;
+	this.tag_array = tag_array;
+	this.initial_selected_tag_array = initial_selected_tag_array;
+
+	this.selected_tag_array = [];
+	this.input_element = document.getElementById(this.id + '_value');
+	this.data_store = new YAHOO.widget.DS_JSArray(this.tag_array);
+	this.array_element = document.getElementById(this.id + '_array');
+
+	YAHOO.util.Event.onContentReady(
+	this.id + '_value', this.handleOnAvailable, this, true);
+}
+
+// }}}
+// {{{ handleOnAvailable()
+
+/**
+ * Sets up the auto-complete widget used by this tag selection control
+ */
+SiteTagEntry.prototype.handleOnAvailable = function()
+{
+	// create auto-complete widget
+	this.auto_complete = new YAHOO.widget.AutoComplete(
+		this.input_element, this.id + '_container', this.data_store, {
+		queryDelay:            0,
+		minQueryLength:        0,
+		highlightClassName:    'site-tag-highlight',
+		prehighlightClassName: 'site-tag-prehighlight',
+		autoHighlight:         true,
+		useShadow:             false,
+		forceSelection:        false,
+		animVert:              false,
+		formatResult:
+			function(item, query)
+			{
+				// 0 is title, 1 is tag string
+				return item[0] + ' (' + item[1] + ')';
+			}
+	});
+
+	this.auto_complete.itemSelectEvent.subscribe(
+		this.addTagFromAutoComplete, this, true);
+
+	YAHOO.util.Event.addListener(this.input_element, 'keydown',
+		function(e, entry) {
+			if (YAHOO.util.Event.getCharCode(e) == 13 &&
+				!entry.auto_complete.isContainerOpen()) {
+				YAHOO.util.Event.stopEvent(e);
+				entry.createTag();
+			}
+		}, this);
+
+	var a_tag = document.createElement('a');
+	a_tag.href = '#';
+	YAHOO.util.Event.addListener(a_tag, 'click',
+		function(e, entry) {
+			YAHOO.util.Event.preventDefault(e);
+			entry.createTag();
+		}, this);
+
+	var img_tag = document.createElement('img');
+	img_tag.src = 'packages/swat/images/swat-tool-link-create.png';
+	img_tag.title = 'Add Tag';
+	img_tag.alt = '';
+	img_tag.className = 'add-tag';
+	a_tag.appendChild(img_tag);
+
+	document.getElementById(this.id).insertBefore(a_tag,
+		document.getElementById(this.id + '_container'));
+
+	// initialize values passed in
+	for (var i = 0; i < this.initial_selected_tag_array.length; i++) {
+		this.addTag(this.initial_selected_tag_array[i]);
+	}
+}
+
+// }}}
+// {{{ addTagFromAutoComplete()
+
+SiteTagEntry.prototype.addTagFromAutoComplete = function(
+	oSelf, elItem, oData)
+{
+	var tag_string = elItem[2][1];
+	this.addTag(tag_string);
+}
+
+// }}}
+// {{{ addTag()
+
+SiteTagEntry.prototype.addTag = function(tag_string)
+{
+	var found = false;
+
+	// trim tag string
+	tag_string = tag_string.replace(/^\s+|\s+$/g, '');
+
+	if (tag_string.length == 0)
+		return;
+
+	for (i = 0; i < this.selected_tag_array.length; i++) {
+		var tag = this.selected_tag_array[i][1];
+		if (tag.toUpperCase() == tag_string.toUpperCase())
+			return;
+	}
+
+	for (i = 0; i < this.data_store.data.length; i++) {
+		var tag = this.data_store.data[i][1];
+		if (tag.toUpperCase() == tag_string.toUpperCase()) {
+			// get tag title
+			var title = this.data_store.data[i][0];
+
+			// remove row from data store
+			var element = this.data_store.data.splice(i, 1);
+
+			// add row to array of selected tags, splice returns an array
+			this.selected_tag_array.push(element[0]);
+
+			found = true;
+			break;
+		}
+	}
+
+	// create new tag
+	var new_tag = (!found);
+
+	if (new_tag)
+		var title = tag_string;
+
+	// create new array node
+	var li_tag = document.createElement('li');
+	li_tag.id = this.id + '_tag_' + tag_string;
+	YAHOO.util.Dom.setStyle(li_tag, 'opacity', 0);
+
+	var anchor_tag = document.createElement('a');
+	anchor_tag.id = this.id + '_tag_remove_' + tag_string;
+	anchor_tag.href = '#';
+	anchor_tag.appendChild(document.createTextNode(
+		SiteTagEntry.remove_text));
+
+	YAHOO.util.Event.addListener(anchor_tag, 'click',
+		function (e)
+		{
+			YAHOO.util.Event.preventDefault(e);
+			this.removeTag(tag_string);
+		},
+		this, true);
+
+	if (new_tag) {
+		var hidden_tag = document.createElement('input');
+		hidden_tag.type = 'hidden';
+		hidden_tag.name = this.id + '_new[]';
+		hidden_tag.value = tag_string;
+		var title_node = document.createTextNode(title + ' (new) ');
+		li_tag.className = 'new-tag';
+		li_tag.appendChild(hidden_tag);
+	} else {
+		var title_node = document.createTextNode(title + ' ');
+		var hidden_tag = document.createElement('input');
+		hidden_tag.type = 'hidden';
+		hidden_tag.name = this.id + '[]';
+		hidden_tag.value = tag_string;
+		li_tag.appendChild(hidden_tag);
+	}
+
+	li_tag.appendChild(title_node);
+	li_tag.appendChild(anchor_tag);
+
+	// add array node
+	this.array_element.appendChild(li_tag);
+
+	var in_attributes = { opacity: { from: 0, to: 1 } };
+	var in_animation = new YAHOO.util.Anim(li_tag, in_attributes,
+		0.5, YAHOO.util.Easing.easeIn);
+
+	in_animation.animate();
+
+	// clear input value once a value is chosen
+	this.input_element.value = '';
+}
+
+// }}}
+// {{{ createTag()
+
+SiteTagEntry.prototype.createTag = function()
+{
+	var tag_title = this.input_element.value;
+	this.addTag(tag_title);
+}
+
+// }}}
+// {{{ removeTag()
+
+SiteTagEntry.prototype.removeTag = function(tag_string)
+{
+	// remove event arrayener
+	var anchor_tag = document.getElementById(
+		this.id + '_tag_remove_' + tag_string);
+
+	if (anchor_tag)
+		YAHOO.util.Event.purgeElement(anchor_tag);
+
+	// remove array node
+	var li_tag = document.getElementById(this.id + '_tag_' + tag_string);
+	if (li_tag) {
+		var out_attributes = { opacity: { from: 1, to: 0 } };
+		var out_animation = new YAHOO.util.Anim(li_tag, out_attributes,
+			0.25, YAHOO.util.Easing.easeOut);
+
+		out_animation.onComplete.subscribe(function(e)
+		{
+			li_tag.parentNode.removeChild(li_tag);
+		}, this, true);
+
+		out_animation.animate();
+	}
+
+	for (i = 0; i < this.selected_tag_array.length; i++) {
+		if (this.selected_tag_array[i][1] == tag_string) {
+			// remove row from selected array
+			var element = this.selected_tag_array.splice(i, 1);
+
+			// add row back to data store, splice returns an array
+			this.data_store.data.push(element[0]);
+			this.data_store.data.sort();
+			break;
+		}
+	}
+}
+
+// }}}
+// {{{ static properties
+
+/**
+ * Remove string resource
+ *
+ * @var string
+ */
+SiteTagEntry.remove_text = 'remove';
+
+// }}}
