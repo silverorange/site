8302363dc50424497147fc34c2479b2e76026349
Michael Gauthier <mike@silverorange.com>
2008-08-14 20:37:31 +0000

    Update WRT [36073].
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@36076 94490950-a86c-4493-9ccb-530f72933e47

--- Site/SiteSidebar.php
+++ Site/SiteSidebar.php
@@ -4,7 +4,7 @@ require_once 'Swat/SwatHtmlTag.php';
 require_once 'Swat/SwatWidget.php';
 require_once 'Swat/SwatUIParent.php';
 require_once 'SwatDB/SwatDBReadaheadIterator.php';
-require_once 'Site/SiteGadget.php';
+require_once 'Site/gadgets/SiteGadget.php';
 
 /**
  * Sidebar widget that can contain multiple gadgets
