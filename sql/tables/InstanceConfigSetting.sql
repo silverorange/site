ce584a786dd645b890223c90484b95aa55a7dd72
Nick Burka <nick@silverorange.com>
2008-08-30 15:50:01 +0000

    More on-delete constraints
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@36427 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/InstanceConfigSetting.sql
+++ sql/tables/InstanceConfigSetting.sql
@@ -1,6 +1,6 @@
 create table InstanceConfigSetting (
 	id serial,
-	instance integer not null references Instance(id),
+	instance integer not null references Instance(id) on delete cascade,
 	name varchar(255) not null,
 	value varchar(1024),
 	primary key (id)
