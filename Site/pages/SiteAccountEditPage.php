7d4c8d98c940db392e770888af0017b84adc3d6d
Michael Gauthier <mike@silverorange.com>
2008-10-09 15:34:53 +0000

    Remove commented out code
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@37723 94490950-a86c-4493-9ccb-530f72933e47

--- Site/pages/SiteAccountEditPage.php
+++ Site/pages/SiteAccountEditPage.php
@@ -136,10 +136,6 @@ class SiteAccountEditPage extends SiteDBEditPage
 
 	protected function updateAccount(SwatForm $form)
 	{
-/*		if (!$this->app->session->isLoggedIn()) {
-			$account->setPassword(
-				$this->ui->getWidget('password')->value);
-		}*/
 		$this->assignUiValuesToObject($this->account, array(
 			'fullname',
 			'email',
