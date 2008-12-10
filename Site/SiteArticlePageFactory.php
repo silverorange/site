8b14b73bb25e2af63a6461383913a6bc2da4939e
Nathan Fredrickson <nathan@silverorange.com>
2008-12-10 19:04:17 +0000

    Reverse decorating order as discussed.  The default article page will now execute first.  An array of decorators will execute from left to right.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@40522 94490950-a86c-4493-9ccb-530f72933e47

--- Site/SiteArticlePageFactory.php
+++ Site/SiteArticlePageFactory.php
@@ -130,14 +130,9 @@ class SiteArticlePageFactory extends SitePageFactory
 		array $arguments)
 	{
 		$has_article_decorator = false;
-		$decorators = array_reverse($decorators);
 		foreach ($decorators as $decorator) {
-			$page = $this->decorate($page, $decorator);
-			if ($page instanceof SiteArticlePage) {
+			if ($page instanceof SiteArticlePage)
 				$has_article_decorator = true;
-				$page->setPath($path);
-				$page->setArticle($article);
-			}
 		}
 
 		// add article decorator if none was defined in the page map
@@ -145,6 +140,14 @@ class SiteArticlePageFactory extends SitePageFactory
 			$page = $this->decorate($page, $this->default_article_page);
 			$page->setPath($path);
 			$page->setArticle($article);
+		}
+
+		foreach ($decorators as $decorator) {
+			$page = $this->decorate($page, $decorator);
+			if ($page instanceof SiteArticlePage) {
+				$page->setPath($path);
+				$page->setArticle($article);
+			}
 		}
 
 		return $page;
