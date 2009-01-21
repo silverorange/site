1e5e81abfcac6b1a67c079b7275abc4b742e7318
Charles Waddell <charles@silverorange.com>
2009-01-21 15:52:20 +0000

    Fix a query to work when the site instance is null.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@41729 94490950-a86c-4493-9ccb-530f72933e47

--- Site/pages/SiteAccountResetPasswordPage.php
+++ Site/pages/SiteAccountResetPasswordPage.php
@@ -101,9 +101,12 @@ class SiteAccountResetPasswordPage extends SiteArticlePage
 		$sql = sprintf('select id from Account where password_tag = %s',
 			$this->app->db->quote($password_tag, 'text'));
 
-		if ($this->app->hasModule('SiteMultipleInstanceModule'))
-			$sql.= sprintf(' and instance = %s', $this->app->db->quote(
-				$this->app->instance->getInstance()->id), 'integer');
+		if ($this->app->hasModule('SiteMultipleInstanceModule')) {
+			$instance_id = $this->app->instance->getId();
+			$sql.= sprintf(' and instance %s %s',
+				SwatDB::equalityOperator($instance_id),
+				$this->app->db->quote($instance_id, 'integer'));
+		}
 
 		return SwatDB::queryOne($this->app->db, $sql);
 	}
