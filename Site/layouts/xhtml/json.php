f730f0451d6377d5d322ea7ef5c95a7842f5eb34
Michael Gauthier <mike@silverorange.com>
2009-03-04 00:00:23 +0000

    Change mime-type and add a note.
    svn commit r43227

--- Site/layouts/xhtml/json.php
+++ Site/layouts/xhtml/json.php
@@ -1,7 +1,10 @@
 <?php
 
-// Set content type to text/javascript
-header('Content-type: text/javascript; charset=utf-8');
+// Note: This mime-type is not official and will prevent JSON content from
+//       being viewable in most browsers. It is, however, a defacto standard
+//       and YUI expects JSON requests to use this mime-type. Other
+//       alternatice mime-types are text/javascript, or text/plain.
+header('Content-Type: application/json; charset=utf-8');
 
 echo $this->content;
 
