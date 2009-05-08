5151d085eb6ecaa7536bd45247f90905857bd9b8
Nathan Fredrickson <nathan@silverorange.com>
2009-05-08 17:22:17 +0000

    chgrp on the file name, not the handle
    svn commit r45460

--- Site/SiteErrorLogger.php
+++ Site/SiteErrorLogger.php
@@ -113,7 +113,7 @@ class SiteErrorLogger extends SwatErrorLogger
 			fclose($log_file);
 
 			if ($this->unix_group !== null)
-				chgrp($log_file, $this->unix_group);
+				chgrp($log_filepath, $this->unix_group);
 		}
 
 		if ($this->base_uri === null)
