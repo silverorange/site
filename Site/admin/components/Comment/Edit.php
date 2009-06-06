1d3f5c50d05c8d4fd91335b4d7ade6afc2a04d76
Nick Burka <nick@silverorange.com>
2009-06-06 15:47:42 +0000

    More centralization of comment code
    svn commit r46185

---
+++ Site/admin/components/Comment/Edit.php
@@ -0,0 +1,140 @@
+<?php
+
+require_once 'Swat/SwatDate.php';
+require_once 'Admin/exceptions/AdminNotFoundException.php';
+require_once 'Admin/pages/AdminDBEdit.php';
+require_once 'NateGoSearch/NateGoSearch.php';
+require_once 'Site/dataobjects/SiteComment.php';
+
+/**
+ * Page for editing comments
+ *
+ * @package   Site
+ * @copyright 2009 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteCommentEdit extends AdminDBEdit
+{
+	// {{{ protected properties
+
+	/**
+	 * @var string
+	 */
+	protected $ui_xml = 'Site/admin/components/Comment/edit.xml';
+
+	/**
+	 * @var SiteComment
+	 */
+	protected $comment;
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+
+		$this->ui->loadFromXML($this->ui_xml);
+		$this->initComment();
+	}
+
+	// }}}
+	// {{{ protected function initComment()
+
+	protected function initComment()
+	{
+		$class_name = SwatDBClassMap::get('SiteComment');
+		$this->comment = new $class_name();
+		$this->comment->setDatabase($this->app->db);
+
+		if (!$this->comment->load($this->id, $this->app->getInstance())) {
+			throw new AdminNotFoundException(
+				sprintf('Comment with id ‘%s’ not found.', $this->id));
+		}
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function saveDBData()
+
+	protected function saveDBData()
+	{
+		$values = $this->ui->getValues(array(
+			'fullname',
+			'link',
+			'email',
+			'bodytext',
+			'status',
+		));
+
+		if ($this->comment->id === null) {
+			$now = new SwatDate();
+			$now->toUTC();
+			$this->comment->createdate   = $now;
+		}
+
+		$this->comment->fullname = $values['fullname'];
+		$this->comment->link     = $values['link'];
+		$this->comment->email    = $values['email'];
+		$this->comment->status   = $values['status'];
+
+		if ($this->comment->status === null) {
+			$this->comment->status = SiteComment::STATUS_PUBLISHED;
+		}
+
+		$this->comment->bodytext = $values['bodytext'];
+
+		if ($this->comment->isModified()) {
+			$this->comment->save();
+
+			$this->addToSearchQueue();
+			$this->clearCache();
+
+			$message = new SwatMessage(Site::_('Comment has been saved.'));
+
+			$this->app->messages->add($message);
+		}
+	}
+
+	// }}}
+	// {{{ protected function addToSearchQueue()
+
+	protected function addToSearchQueue()
+	{
+	}
+
+	// }}}
+	// {{{ protected function clearCache()
+
+	protected function clearCache()
+	{
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+
+		$statuses = SiteComment::getStatusArray();
+		$this->ui->getWidget('status')->addOptionsByArray($statuses);
+	}
+
+	// }}}
+	// {{{ protected function loadDBData()
+
+	protected function loadDBData()
+	{
+		$this->ui->setValues(get_object_vars($this->comment));
+	}
+
+	// }}}
+}
+
+?>
