64cff01815f4f2d52d2cabba6fd1e5696cc754b0
Charles Waddell <charles@silverorange.com>
2009-07-29 17:52:20 +0000

    Allow config settings to be set as default. Refs #4693
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@47547 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/InstanceConfigSetting.sql
+++ sql/tables/InstanceConfigSetting.sql
@@ -1,9 +1,11 @@
-create table InstanceConfigSetting (
-	id serial,
-	instance integer not null references Instance(id) on delete cascade,
+create table NewInstanceConfigSetting (
 	name varchar(255) not null,
 	value varchar(1024),
-	primary key (id)
+	is_default boolean not null default false,
+	instance integer not null references Instance(id) on delete cascade,
+	primary key (name, is_default, instance)
 );
 
-CREATE INDEX InstanceConfigSetting_name_index ON InstanceConfigSetting(name);
+CREATE INDEX NewInstanceConfigSetting_name_index ON NewInstanceConfigSetting(name);
+CREATE INDEX NewInstanceConfigSetting_is_default_index ON NewInstanceConfigSetting(is_default);
+CREATE INDEX NewInstanceConfigSetting_instance_index ON NewInstanceConfigSetting(instance);
