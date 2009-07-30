cb435ee3feda33104a68d30506289e0d92ac69b5
Charles Waddell <charles@silverorange.com>
2009-07-30 14:13:13 +0000

    An instance config setting editor.
    svn commit r47588

---
+++ Site/admin/components/InstanceSetting/include/SiteAbstractConfigPage.php
@@ -0,0 +1,121 @@
+<?php
+
+require_once 'Swat/SwatUI.php';
+
+/**
+ * Abstract page that initilizes, loads and saves a user interface based on
+ * current configuration settings.
+ *
+ * @package   Site
+ * @copyright 2009 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+abstract class SiteAbstractConfigPage
+{
+	// {{{ protected properties
+
+	/**
+	 * @var SwatUI
+	 */
+	protected $ui;
+
+	// }}}
+	// {{{ public function getUI()
+
+	public function getUI()
+	{
+		return $this->ui;
+	}
+
+	// }}}
+	// {{{ public function initUI()
+
+	public function initUI()
+	{
+		$this->ui = new SwatUI();
+		$this->ui->loadFromXML($this->getPathToXML());
+
+		foreach ($this->getConfigSettings() as $section => $keys) {
+			foreach ($keys as $name) {
+				$field_name = $section.'_'.$name;
+				$widget = $this->ui->getWidget($field_name);
+				$method = 'init'.str_replace(' ', '',
+					ucwords(str_replace('_', ' ', $field_name)));
+
+				if (method_exists($this, $method)) {
+					$this->$method($widget);
+				}
+			}
+		}
+
+		$this->ui->init();
+	}
+
+	// }}}
+	// {{{ public function saveUI()
+
+	public function saveUI(SiteConfigModule $config)
+	{
+		$changed_settings = array();
+
+		foreach ($this->getConfigSettings() as $section => $keys) {
+			foreach ($keys as $name) {
+				$field_name = $section.'_'.$name;
+				$widget = $this->ui->getWidget($field_name);
+				$method = 'save'.str_replace(' ', '',
+					ucwords(str_replace('_', ' ', $field_name)));
+
+				if (method_exists($this, $method)) {
+					if ($this->$method($config, $widget)) {
+						$changed_settings[] = $section.'.'.$name;
+					}
+				} else if ($config->$section->$name !== $widget->value) {
+					$config->$section->$name = $widget->value;
+					$changed_settings[] = $section.'.'.$name;
+				}
+			}
+		}
+
+		return $changed_settings;
+	}
+
+	// }}}
+	// {{{ public function loadUI()
+
+	public function loadUI(SiteConfigModule $config)
+	{
+		foreach ($this->getConfigSettings() as $section => $keys) {
+			foreach ($keys as $name) {
+				$field_name = $section.'_'.$name;
+				$widget = $this->ui->getWidget($field_name);
+				$method = 'load'.str_replace(' ', '',
+					ucwords(str_replace('_', ' ', $field_name)));
+
+				if (method_exists($this, $method)) {
+					$this->$method($config, $widget);
+				} else {
+					$widget->value = $config->$section->$name;
+				}
+			}
+		}
+	}
+
+	// }}}
+	// {{{ abstract public function getPageTitle()
+
+	abstract public function getPageTitle();
+
+	// }}}
+	// {{{ abstract public function getConfigSettings()
+
+	abstract public function getConfigSettings();
+
+	// }}}
+	// {{{ abstract protected function getPathToXML()
+
+	abstract protected function getPathToXML();
+
+	// }}}
+}
+
+?>
---
+++ Site/admin/components/InstanceSetting/include/SiteConfigPage.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once dirname(__FILE__).'/SiteAbstractConfigPage.php';
+
+/**
+ * Config page used for displaying and saving config settings for the Site
+ * package.
+ *
+ * @package   Site
+ * @copyright 2009 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteConfigPage extends SiteAbstractConfigPage
+{
+	// {{{ public function getPageTitle()
+
+	public function getPageTitle()
+	{
+		return 'Site Settings';
+	}
+
+	// }}}
+	// {{{ public function getConfigSettings()
+
+	public function getConfigSettings()
+	{
+		return array(
+			'site' => array(
+				'title',
+				'meta_description',
+			),
+			'comment' => array(
+				'akismet_key',
+			),
+			'date' => array(
+				'time_zone',
+			),
+		);
+	}
+
+	// }}}
+	// {{{ protected function getPathToXML()
+
+	protected function getPathToXML()
+	{
+		return dirname(__FILE__).'/config-page.xml';
+	}
+
+	// }}}
+}
+
+?>
