77e3e23a71b0c7c51da58c98ee3c8de716e2a65f
Michael Gauthier <mike@silverorange.com>
2010-01-05 16:11:34 +0000

    Add ability to display pagination for search results when total count is only known to a certain limit. Depends on r51989 of Swat.
    svn commit r51990

--- Site/SiteSearchPagination.php
+++ Site/SiteSearchPagination.php
@@ -11,8 +11,11 @@ require_once 'Swat/SwatPagination.php';
  * diffirentiate between two SiteSearchPagination widgets on the same
  * search results page.
  *
+ * This widget also supports pagination of result sets where the total count is
+ * only known accurately up to a certain point.
+ *
  * @package   Site
- * @copyright 2004-2007 silverorange
+ * @copyright 2004-2010 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class SiteSearchPagination extends SwatPagination
@@ -27,7 +30,13 @@ class SiteSearchPagination extends SwatPagination
 	public $unset_get_vars = array();
 
 	/**
-	 * Optional string to identify the type of content being paged with this widget
+	 * @var integer
+	 */
+	public $max_accurate_records;
+
+	/**
+	 * Optional string to identify the type of content being paged with this
+	 * widget
 	 *
 	 * @var string
 	 */
@@ -100,6 +109,110 @@ class SiteSearchPagination extends SwatPagination
 		$link.= 'page=%s';
 
 		return $link;
+	}
+
+	// }}}
+	// {{{ protected function displayPages()
+
+	/**
+	 * Displays a smart list of pages
+	 */
+	protected function displayPages()
+	{
+		$j = 0;
+
+		$link = $this->getLink();
+
+		$anchor = new SwatHtmlTag('a');
+		$span = new SwatHtmlTag('span');
+		$current = new SwatHtmlTag('span');
+		$current->class = 'swat-pagination-current';
+
+		$total_pages = max($this->total_pages, $this->current_page);
+
+		for ($i = 1; $i <= $total_pages; $i++) {
+			$display = false;
+
+			if ($this->current_page < 7 && $i <= 10) {
+				// Current page is in the first 6, show the first 10 pages
+				$display = true;
+
+			} elseif ($this->current_page > $this->total_pages - 6 &&
+				$i >= $this->total_pages - 10 &&
+				($this->max_accurate_records === null ||
+				$this->total_records <= $this->max_accurate_records)) {
+
+				// Current page is in the last 6, show the last 10 pages
+				$display = true;
+
+			} elseif ($i < 3 ||
+				($i > $this->total_pages - 2 &&
+				($this->max_accurate_records === null ||
+				$this->total_records <= $this->max_accurate_records)) ||
+				abs($this->current_page - $i) <= 3) {
+
+				// Always show the first 2, last 2, and middle 6 pages
+				$display = true;
+			}
+
+			if ($display) {
+				if ($j + 1 != $i) {
+					// ellipses
+					$span->setContent('…');
+					$span->display();
+				}
+
+				if ($i == $this->current_page) {
+					$current->setContent((string)$i);
+					$current->display();
+				} else {
+					$anchor->href = sprintf($link, (string)$i);
+					$anchor->title =
+						sprintf(Swat::_('Go to page %d'), ($i));
+
+					$anchor->setContent((string)($i));
+					$anchor->display();
+				}
+
+				$j = $i;
+			}
+		}
+
+		if ($this->max_accurate_records !== null && $this->total_records > $this->max_accurate_records) {
+			// ellipses
+			$span->setContent('…');
+			$span->display();
+		}
+	}
+
+	// }}}
+	// {{{ protected function calculatePages()
+
+	/**
+	 * Calculates page totals
+	 *
+	 * Sets the internal total_pages, next_page and prev_page properties.
+	 */
+	protected function calculatePages()
+	{
+		$records = min($this->max_accurate_records, $this->total_records);
+
+		$this->total_pages = ceil($records / $this->page_size);
+
+		if (($this->total_pages <= 1) ||
+			($this->total_pages == $this->current_page &&
+			$this->max_accurate_records !== null &&
+			$this->total_records <= $this->max_accurate_records)) {
+			$this->next_page = 0;
+		} else {
+			$this->next_page = $this->current_page + 1;
+		}
+
+		if ($this->current_page > 0) {
+			$this->prev_page = $this->current_page - 1;
+		} else {
+			$this->prev_page = 0;
+		}
 	}
 
 	// }}}
