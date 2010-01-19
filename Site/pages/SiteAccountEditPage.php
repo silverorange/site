90d6390f18efb79b8d4b90f57869da5ee2fdbd96
Isaac Grant <isaac@silverorange.com>
2010-01-19 01:49:46 +0000

    break out the building of the message for invalid emails to its own method for subclass use.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@52222 94490950-a86c-4493-9ccb-530f72933e47

--- Site/pages/SiteAccountEditPage.php
+++ Site/pages/SiteAccountEditPage.php
@@ -117,23 +117,31 @@ class SiteAccountEditPage extends SiteDBEditPage
 		$found = $account->loadWithEmail($email->value, $instance);
 
 		if ($found && $this->account->id !== $account->id) {
-			$message = new SwatMessage(
-				Site::_('An account already exists with this email address.'),
-				'error');
-
-			if ($this->isNew($this->ui->getWidget('edit_form'))) {
-				$email_link = sprintf(
-					'<a href="account/forgotpassword?email=%s">',
-					$email->value);
-
-				$message->secondary_content =
-					sprintf(Site::_('You can %srequest a new password%s to '.
-						'log into the existing account.'), $email_link, '</a>');
-			}
-
-			$message->content_type = 'text/xml';
+			$message = $this->getInvalidEmailMessage();
 			$email->addMessage($message);
 		}
+	}
+
+	// }}}
+	// {{{ protected function getInvalidEmailMessage()
+
+	protected function getInvalidEmailMessage()
+	{
+		$message = new SwatMessage(
+			Site::_('An account already exists with this email address.'),
+			'error');
+
+		if ($this->isNew($this->ui->getWidget('edit_form'))) {
+			$email_link = sprintf('<a href="account/forgotpassword?email=%s">',
+				$email->value);
+
+			$message->secondary_content =
+				sprintf(Site::_('You can %srequest a new password%s to '.
+					'log into the existing account.'), $email_link, '</a>');
+		}
+
+		$message->content_type = 'text/xml';
+		return $message;
 	}
 
 	// }}}
