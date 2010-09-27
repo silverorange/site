9993e3951f32771a927ae4573e55350c11b98fa5
Charles Waddell <charles@silverorange.com>
2010-09-27 20:29:11 +0000

    Add CDN support to SiteImage. Now when an image is saved a copy is also added to whatever CDN is configured.
    svn commit r57680

---
+++ Site/SiteCDN.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Interface that defines some basic operations you can preform to on a CDN.
+ *
+ * @package   Site
+ * @copyright 2010 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+interface SiteCDN
+{
+	// {{{ public function copyToCDN()
+
+	public function copyToCDN($source, $destination);
+
+	// }}}
+	// {{{ public function deleteFromCDN()
+
+	public function deleteFromCDN($file_path);
+
+	// }}}
+}
+
+?>
