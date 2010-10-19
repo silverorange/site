5482759d4fdb9d9e6429ea60aaa831dffbc90ed1
Michael Gauthier <mike@silverorange.com>
2010-10-19 20:04:37 +0000

    New table and dataobjects for storing contact messages in the database before sending on SiteContactPage.
    svn commit r58105

---
+++ Site/dataobjects/SiteContactMessageWrapper.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'SwatDB/SwatDBRecordsetWrapper.php';
+require_once 'Site/dataobjects/SiteContactMessage.php';
+
+/**
+ * A recordset wrapper class for SiteContactMessage objects
+ *
+ * @package   Site
+ * @copyright 2010 silverorange
+ * @see       SiteContactMessage
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteCommentWrapper extends SwatDBRecordsetWrapper
+{
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+		$this->row_wrapper_class = SwatDBClassMap::get('SiteContactMessage');
+		$this->index_field = 'id';
+	}
+
+	// }}}
+}
+
+?>
