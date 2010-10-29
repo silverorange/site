d270fe19f39e9fe190f6397be90e8ea0b03b63b5
Charles Waddell <charles@silverorange.com>
2010-10-29 19:07:31 +0000

    Use camel case and better method names. Refs #2684
    svn commit r58391

--- Site/SiteCdn.php
+++ Site/SiteCdn.php
@@ -1,22 +1,34 @@
 <?php
 
 /**
- * Interface that defines some basic operations you can preform to on a CDN.
+ * Interface that defines some basic operations that can be preformed on a CDN.
  *
  * @package   Site
  * @copyright 2010 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-interface SiteCDN
+interface SiteCdn
 {
-	// {{{ public function copyToCDN()
+	// {{{ public function copyFile()
 
-	public function copyToCDN($source, $destination, $mime_type = null);
+	/**
+	 * Copies a file to the CDN
+	 *
+	 * @param string $source the source path of the file to copy.
+	 * @param string $destination the destination path of the file to copy.
+	 * @param string $mime_type the MIME type of the file. Defaults to null.
+	 */
+	public function copyFile($source, $destination, $mime_type = null);
 
 	// }}}
-	// {{{ public function deleteFromCDN()
+	// {{{ public function deleteFile()
 
-	public function deleteFromCDN($file_path, $mime_type = null);
+	/**
+	 * Deletes a file from the CDN
+	 *
+	 * @param string $file_path the path, on the CDN, of the file to delete.
+	 */
+	public function deleteFile($file_path);
 
 	// }}}
 }
