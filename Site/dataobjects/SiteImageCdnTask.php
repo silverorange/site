4fba5d50e635e43671eac4f23adff72ef1928c88
Charles Waddell <charles@silverorange.com>
2010-11-01 19:34:25 +0000

    Move some processing methods away from the dataobjects.
    svn commit r58421

--- Site/dataobjects/SiteImageCdnTask.php
+++ Site/dataobjects/SiteImageCdnTask.php
@@ -1,8 +1,14 @@
 <?php
 
 require_once 'SwatDB/SwatDBDataObject.php';
-require_once 'Site/exceptions/SiteNotFoundException.php';
 
+/**
+ * A task that should be preformed to a CDN in the near future
+ *
+ * @package   Site
+ * @copyright 2010 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
 class SiteImageCdnTask extends SwatDBDataObject
 {
 	// {{{ public properties
@@ -23,130 +29,28 @@ class SiteImageCdnTask extends SwatDBDataObject
 	public $image_path;
 
 	// }}}
-	// {{{ protected properties
-
-	/**
-	 * @var SiteImage
-	 */
-	protected $image;
-
-	// }}}
-	// {{{ public function execute()
-
-	/**
-	 * Executes this task
-	 */
-	public function execute(SiteCdn $cdn)
-	{
-		try {
-			switch ($this->operation) {
-				case 'copy':
-					$this->copyImage($cdn);
-					break;
-				case 'delete':
-					$this->deleteImage($cdn);
-					break;
-			}
-
-			$this->delete();
-		} catch (SiteNotFoundException $e) {
-			$this->delete();
-		} catch (Services_Amazon_S3_Exception $e) {
-			$exception = new SwatException($e);
-			$exception->process(false);
-		}
-	}
-
-	// }}}
 	// {{{ protected function init()
 
 	protected function init()
 	{
-		$this->table    = 'ImageCDNQueue';
+		$this->table    = 'ImageCdnQueue';
 		$this->id_field = 'integer:id';
-	}
-
-	// }}}
-	// {{{ protected function copyImage()
-
-	/**
-	 * Copies this taks's image to a CDN
-	 *
-	 * @param SiteCdn $cdn the cdn to copy the image to.
-	 */
-	protected function copyImage(SiteCdn $cdn)
-	{
-		$image = $this->getImage();
-		$shortname = $this->getDimensionShortname();
-
-		$cdn->copyFile(
-			$image->getFilePath($shortname),
-			$this->image_path,
-			$image->getMimeType($shortname));
-
-		$image->setOnCDN(true, $shortname);
-	}
-
-	// }}}
-	// {{{ protected function deleteImage()
-
-	/**
-	 * Deletes this taks's image to a CDN
-	 *
-	 * @param SiteCdn $cdn the cdn to delete the image from.
-	 */
-	protected function deleteImage(SiteCdn $cdn)
-	{
-		try {
-			$image = $this->getImage();
-			$image->setOnCDN(false, $this->getDimensionShortname());
-		} catch (SiteNotFoundException $e) {
-		}
-
-		$cdn->deleteFile($this->image_path);
-	}
-
-	// }}}
-	// {{{ protected function getImage()
-
-	/**
-	 * Gets this task's image
-	 *
-	 * @return SiteImage this task's image.
-	 */
-	protected function getImage()
-	{
-		if (!($this->image instanceof SiteImage)) {
-			$class_name = SwatDBClassMap::get('SiteImage');
-
-			$image = new $class_name();
-			$image->setDatabase($this->db);
-			$image->setFileBase('../../www/images');
-
-			if (!($image->load(intval($this->getImageId())))) {
-				throw new SiteNotFoundException();
-			}
-
-			$this->image = $image;
-		}
-
-		return $this->image;
 	}
 
 	// }}}
 	// {{{ protected function getImageId()
 
 	/**
-	 * Gets this task's image id
+	 * Gets the id of this task's image
 	 *
-	 * @return string this task's image id.
+	 * @return integer the id of this task's image.
 	 */
 	protected function getImageId()
 	{
 		$ruins  = explode('/', $this->image_path);
 		$debris = explode('.', $ruins[3]);
 
-		return $debris[0];
+		return intval($debris[0]);
 	}
 
 	// }}}
