7025388af05886f4723e5f535ed77c159f0ff7db
Isaac Grant <isaac@silverorange.com>
2010-11-05 15:15:08 +0000

    add the index field so that it can be used with loadSubDataobjects
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@58508 94490950-a86c-4493-9ccb-530f72933e47

--- Site/dataobjects/SiteImageDimensionWrapper.php
+++ Site/dataobjects/SiteImageDimensionWrapper.php
@@ -7,7 +7,7 @@ require_once 'Site/dataobjects/SiteImageDimension.php';
  * A recordset wrapper class for SiteImageDimension objects
  *
  * @package   Site
- * @copyright 2008 silverorange
+ * @copyright 2008-2010 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  * @see       SiteImageDimension
  */
@@ -20,6 +20,7 @@ class SiteImageDimensionWrapper extends SwatDBRecordsetWrapper
 		parent::init();
 
 		$this->row_wrapper_class = SwatDBClassMap::get('SiteImageDimension');
+		$this->index_field = 'id';
 	}
 
 	// }}}
