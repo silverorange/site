7c1831795cf44e8eaf651ca53bd6b7b176553415
Michael Gauthier <mike@silverorange.com>
2010-11-10 21:12:40 +0000

    Remove files from Site that were moved to Deliverance.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@58628 94490950-a86c-4493-9ccb-530f72933e47

--- sql/pgsql/tables/MailingListUnsubscribeQueue.sql
+++
@@ -1,7 +0,0 @@
-create table MailingListUnsubscribeQueue (
-	id serial,
-	email varchar(255) not null,
-	primary key (id)
-);
-
-create index MailingListUnsubscribeQueue_email_index on MailingListUnsubscribeQueue(email);
