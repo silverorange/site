5bfee50bc29741cebb32949f906f8ee07ab64f70
Isaac Grant <isaac@silverorange.com>
2010-11-10 21:10:11 +0000

    add a bunch of things we did in older s3 copying code.  - optionally use an md5 to prevent copying already existing files over a second time.  - set cache control max age.  - grab the mime_type from the file if it isn't passed in.
    svn commit r58625

--- Site/SiteAmazonCdnModule.php
+++ Site/SiteAmazonCdnModule.php
@@ -48,6 +48,40 @@ class SiteAmazonCdnModule extends SiteApplicationModule implements SiteCdn
 	 */
 	protected $bucket;
 
+	/**
+	 * Max age of for the Cache Control header.
+	 *
+	 * In seconds, defaults to 1 hour (cloudfront's minimum cache time).
+	 *
+	 * @var integer
+	 */
+	protected $max_age = 3600;
+
+	/**
+	 * Whether or not to check the md5 of a file when saving.
+	 *
+	 * If true, check to see if the object already exists and checks the current
+	 * file's md5 against the new file's md5 before copying. Defaults to true.
+	 *
+	 * md5 is always saved in a custom metadata field. ETag isn't used because
+	 * amazon doesn't guarantee it to always be the file's md5, even though it
+	 * appears as though it always is.
+	 *
+	 * @var boolean
+	 */
+	protected $check_md5 = true;
+
+	/**
+	 * Access control list for the object
+	 *
+	 * This may be one of the predefined ACLs specified by a
+	 * Services_Amazon_S3_AccessControlList::ACL_xxx constant, or a
+	 * Services_Amazon_S3_AccessControlList instance.
+	 *
+	 * @var string|Services_Amazon_S3_AccessControlList
+	 */
+	protected $acl = Services_Amazon_S3_AccessControlList::ACL_PUBLIC_READ;
+
 	// }}}
 	// {{{ public function init()
 
@@ -64,6 +98,45 @@ class SiteAmazonCdnModule extends SiteApplicationModule implements SiteCdn
 	}
 
 	// }}}
+	// {{{ public function setCheckMd5()
+
+	/**
+	 * Sets whether or not to check the md5 field;
+	 *
+	 * @param boolean $check_md5 true if we want to check it, false if we don't.
+	 */
+	public function setCheckMd5($check_md5 = true)
+	{
+		$this->check_md5 = $check_md5;
+	}
+
+	// }}}
+	// {{{ public function setMaxAge()
+
+	/**
+	 * Sets maxium age for cache control
+	 *
+	 * @param integer $max_age the maximum age of the resource in seconds.
+	 */
+	public function setMaxAge($max_age)
+	{
+		$this->max_age = $max_age;
+	}
+
+	// }}}
+	// {{{ public function setAcl()
+
+	/**
+	 * Sets maxium age for cache control
+	 *
+	 * @param integer $max_age the maximum age of the resource in seconds.
+	 */
+	public function setAcl($acl)
+	{
+		$this->acl = $acl;
+	}
+
+	// }}}
 	// {{{ public function copyFile()
 
 	/**
@@ -71,30 +144,66 @@ class SiteAmazonCdnModule extends SiteApplicationModule implements SiteCdn
 	 *
 	 * @param string $source the source path of the file to copy.
 	 * @param string $destination the destination path of the file to copy.
-	 * @param string $mime_type the MIME type of the file. Defaults to null.
+	 * @param string $mime_type the MIME type of the file. If null, we grab the
+	 *                           MIME type from the file. Defaults to null.
+	 * @param integer $max_age the maximum age for cache control. If set
+	 *                          overrides $this->max_age. Defaults to null.
+	 *
+	 * @returns boolean $copy whether or not the file has been copied. If false
+	 *                         this means the file was already on s3.
 	 */
-	public function copyFile($source, $destination, $mime_type = null)
+	public function copyFile($source, $destination, $mime_type = null,
+		$max_age = null)
 	{
 		if (file_exists($source) === false) {
 			throw new SwatFileNotFoundException(Site::_(sprintf(
 				'File missing ‘%s’.', $source)));
 		}
 
-		$s3_object = $this->bucket->getObject($destination);
-		$s3_object->data = file_get_contents($source);
-		$s3_object->acl  =
-			Services_Amazon_S3_AccessControlList::ACL_PUBLIC_READ;
-
-		if ($mime_type != '') {
-			$s3_object->contentType = $mime_type;
-		}
-
-		if ($s3_object->data === false) {
+		$file = file_get_contents($source);
+		if ($file === false) {
 			throw new SwatFileNotFoundException(Site::_(sprintf(
 				'Unable to open file ‘%s’.', $source)));
 		}
 
-		$s3_object->save();
+		$s3_object = $this->bucket->getObject($destination);
+		$md5       = md5($file);
+		$copy      = true;
+
+		if ($this->check_md5 == true) {
+			// check for existing object.
+			if ($s3_object->load(
+				Services_Amazon_S3_Resource_Object::LOAD_METADATA_ONLY)) {
+				if (array_key_exists('md5', $s3_object->userMetadata)) {
+					if ($s3_object->userMetadata['md5'] == $md5) {
+						$copy = false;
+					}
+				}
+			}
+		}
+
+		if ($copy === true) {
+			if ($mime_type === null) {
+				$finfo     = finfo_open(FILEINFO_MIME);
+				$mime_type = finfo_file($finfo, $from);
+			}
+
+			if ($max_age === null) {
+				$max_age = $this->max_age;
+			}
+
+			$s3_object->data         = $file;
+			$s3_object->contentType  = $mime_type;
+			$s3_object->acl          = $this->acl;
+			$s3_object->userMetadata = array('md5' => $md5);
+			$s3_object->httpHeaders  = array(
+				'cache-control' => 'max-age='.$max_age,
+				);
+
+			$s3_object->save();
+		}
+
+		return $copy;
 	}
 
 	// }}}
