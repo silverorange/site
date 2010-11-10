339aecfd4eb4ad31a18629bb0d95fcd06cb9b4b3
Michael Gauthier <mike@silverorange.com>
2010-11-10 21:12:40 +0000

    Remove files from Site that were moved to Deliverance.
    svn commit r58628

--- sql/pgsql/tables/MailingListMemberCache.sql
+++
@@ -1,7 +0,0 @@
-create table MailingListMemberCache (
-	id serial,
-	email varchar(255) not null,
-	primary key (id)
-);
-
-create index MailingListMemberCache_email_index on MailingListMemberCache(email);
--- sql/pgsql/tables/MailingListSubscribeQueue.sql
+++
@@ -1,9 +0,0 @@
-create table MailingListSubscribeQueue (
-	id serial,
-	email varchar(255) not null,
-	info text,
-	send_welcome boolean not null default false,
-	primary key (id)
-);
-
-create index MailingListSubscribeQueue_email_index on MailingListSubscribeQueue(email);
