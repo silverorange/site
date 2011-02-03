710924e2aca3ee66b3abab00d65e256d76cb2c2c
Nathan Fredrickson <nathan@silverorange.com>
2011-02-03 21:49:45 +0000

    Add a way to optionally limit articles per instance.
    svn commit r60134

--- sql/pgsql/views/EnabledArticleView.sql
+++ sql/pgsql/views/EnabledArticleView.sql
@@ -1,3 +1,6 @@
 create or replace view EnabledArticleView as
-	select id from Article
+	select id, ArticleInstanceBinding.instance
+		from Article
+			left outer join ArticleInstanceBinding on
+				Article.id = ArticleInstanceBinding.article
 	where enabled = true;
