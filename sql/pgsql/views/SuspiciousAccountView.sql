1c0bb0eed7dc29d86055f09966859e6b8942de33
Michael Gauthier <mike@silverorange.com>
2011-03-02 17:15:07 +0000

    Update suspicious account activity to check activity in the last week. We lose the ability to count logins, user_agents and ip_addresses but it's more useful as you don't have to monitor it every hour.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@60641 94490950-a86c-4493-9ccb-530f72933e47

--- sql/pgsql/views/SuspiciousAccountView.sql
+++ sql/pgsql/views/SuspiciousAccountView.sql
@@ -1,11 +1,30 @@
 create or replace view SuspiciousAccountView as
 	select
 		account,
-		count(1) as login_count,
-		count(distinct user_agent) as user_agent_count,
-		count(distinct ip_address) as ip_address_count
+		lag(login_date, 2, null) over AccountWindow as last_suspicious_login,
+		case
+			when lag(login_date, 2, null) over AccountWindow - lead(login_date, 2, null) over AccountWindow < interval '1 hour' then true
+			else false
+		end as too_many_logins,
+		case
+			when         lag(ip_address, 2, null) over AccountWindow = ip_address
+				and  lag(ip_address, 1, null) over AccountWindow = ip_address
+				and lead(ip_address, 1, null) over AccountWindow = ip_address
+				and lead(ip_address, 2, null) over AccountWindow = ip_address then false
+			else true
+		end as ip_address_distinct,
+		case
+			when         lag(user_agent, 2, null) over AccountWindow = user_agent
+				and  lag(user_agent, 1, null) over AccountWindow = user_agent
+				and lead(user_agent, 1, null) over AccountWindow = user_agent
+				and lead(user_agent, 2, null) over AccountWindow = user_agent then false
+			else true
+		end as user_agent_distinct
 	from AccountLoginHistory
-	where login_date > now() - interval '1 hour'
-	group by account
-	having count(1) > 5 and
-		(count(distinct user_agent) > 1 or count(distinct ip_address) > 1);
+	where login_date > now() - interval '7 days'
+	window AccountWindow as (
+		partition by account
+		order by login_date desc
+		rows between unbounded preceding and unbounded following
+	)
+	order by account, login_date desc;
