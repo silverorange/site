31794b7935368974b6f6f472264ffdfa55558d0a
Isaac Grant <isaac@silverorange.com>
2011-11-11 09:46:59 +0000

    in my quest for easier filtering of the logs, add a generic SiteCommandLineException. Would be nice to update all command line apps to default to this instead of SiteException.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@66588 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Site/exceptions/SiteCommandLineException.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Site/exceptions/SiteException.php';
+
+/**
+ * Thrown when running a command line application.
+ *
+ * @package   Site
+ * @copyright 2011 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteCommandLineException extends SiteException
+{
+}
+
+?>
