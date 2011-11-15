610c2f7dbb4a27a28037f8b8cf1b2e594443e8c4
Isaac Grant <isaac@silverorange.com>
2011-11-15 16:58:54 +0000

    bring sql up to date with changes brought up from Emrap
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@66708 94490950-a86c-4493-9ccb-530f72933e47

--- sql/pgsql/tables/Attachment.sql
+++ sql/pgsql/tables/Attachment.sql
@@ -1,13 +1,18 @@
 create table Attachment (
 	id serial,
 
-	attachment_set integer not null references AttachmentSet(id) on delete cascade,
+	attachment_set integer not null
+		references AttachmentSet(id) on delete cascade,
+
 	title varchar(255) not null,
+	filename varchar(255),
 	original_filename varchar(255),
 	createdate timestamp,
-	-- assume attachments won't be larger than 5GB
+	-- store file size as a decimal to support large file sizes as we can't
+	-- support bigints. length of field assumes attachments won't be larger than
+	-- 5GB.
 	file_size decimal(10, 0) not null default 0,
-	mime_type varchar(50),
+	mime_type varchar(50) not null,
 	on_cdn boolean not null default false,
 
 	primary key (id)
