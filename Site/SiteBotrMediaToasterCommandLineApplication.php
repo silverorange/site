9ccb826094c1a64ace4bf2ebd45791cb9da59343
Isaac Grant <isaac@silverorange.com>
2011-11-29 16:19:58 +0000

    remove a CoureHost reference that slipped in
    svn commit r67245

--- Site/SiteBotrMediaToasterCommandLineApplication.php
+++ Site/SiteBotrMediaToasterCommandLineApplication.php
@@ -317,21 +317,6 @@ abstract class SiteBotrMediaToasterCommandLineApplication
 	}
 
 	// }}}
-	// {{{ protected function addConfigDefinitions()
-
-	/**
-	 * Adds configuration definitions to the config module of this application
-	 *
-	 * @param SiteConfigModule $config the config module of this application to
-	 *                                  witch to add the config definitions.
-	 */
-	protected function addConfigDefinitions(SiteConfigModule $config)
-	{
-		parent::addConfigDefinitions($config);
-		$config->addDefinitions(CourseHost::getConfigDefinitions());
-	}
-
-	// }}}
 }
 
 // {{{ function SourceFileErrorHandler()
