27d1ae75994f644a6a7bb4938f878ee85e6e352e
Michael Gauthier <mike@silverorange.com>
2012-02-23 20:42:28 +0000

    Make SiteComment review actions the same as the actions displayed on the comment index.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/site/trunk@69356 94490950-a86c-4493-9ccb-530f72933e47

--- Site/admin/SiteCommentApprovalPage.php
+++ Site/admin/SiteCommentApprovalPage.php
@@ -12,11 +12,30 @@ require_once 'Site/dataobjects/SiteComment.php';
  */
 abstract class SiteCommentApprovalPage extends AdminApproval
 {
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+		$this->ui->getWidget('delete_button')->title = Site::_('Deny');
+	}
+
+	// }}}
+
 	// {{{ protected function approve()
 
 	protected function approve()
 	{
 		$this->data_object->status = SiteComment::STATUS_PUBLISHED;
+		$this->data_object->save();
+	}
+
+	// }}}
+	// {{{ protected function delete()
+
+	protected function delete()
+	{
+		$this->data_object->status = SiteComment::STATUS_UNPUBLISHED;
 		$this->data_object->save();
 	}
 
