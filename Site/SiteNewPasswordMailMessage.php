accc5d9f164bb9c47668ed27e54ca4902d640077
Michael Gauthier <mike@silverorange.com>
2012-05-23 19:34:04 +0000

    Same as r72181 but for NewPasswordMailMessage.
    svn commit r72182

--- Site/SiteNewPasswordMailMessage.php
+++ Site/SiteNewPasswordMailMessage.php
@@ -84,20 +84,16 @@ class SiteNewPasswordMailMessage extends SiteMultipartMailMessage
 	 */
 	public function send()
 	{
-		if ($this->account->email === null)
+		if ($this->account->email == '') {
 			throw new SiteException('Account requires an email address to '.
 				'generate new password. Make sure email is loaded on the '.
 				'account object.');
-
-		if ($this->account->getFullname() === null)
-			throw new SiteException('Account requires a fullname to '.
-				'generate new password. Make sure the getFullname() method '.
-				'returns a fullname.');
+		}
 
 		$this->to_address = $this->account->email;
-		$this->to_name = $this->account->getFullname();
-		$this->text_body = $this->getTextBody();
-		$this->html_body = $this->getHtmlBody();
+		$this->to_name    = $this->getFullname();
+		$this->text_body  = $this->getTextBody();
+		$this->html_body  = $this->getHtmlBody();
 
 		parent::send();
 	}
@@ -147,6 +143,21 @@ class SiteNewPasswordMailMessage extends SiteMultipartMailMessage
 			Site::_('After logging into your account, you can set a new '.
 			'password by clicking the "Change Login Password" on your '.
 			'account page.'));
+	}
+
+	// }}}
+	// {{{ protected function getFullname()
+
+	protected function getFullname()
+	{
+		$fullname = $this->getAccount->getFullname();
+
+		if ($fullname == '') {
+			// in case account doesn't have a fullname for some reason
+			$fullname = $this->account->email;
+		}
+
+		return $fullname;
 	}
 
 	// }}}
