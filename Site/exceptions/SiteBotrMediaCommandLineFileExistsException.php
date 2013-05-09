cce78641302a18437455bad18cf07eec52552744
Isaac Grant <isaac@silverorange.com>
2013-05-09 14:23:54 +0000

    make the original downloader work with 64 bit PHP for big file support, and treat files that have already been downloaded differently to make it more robust. Should help get rid of some straglers on BOTR
    svn commit r80871

---
+++ Site/exceptions/SiteBotrMediaCommandLineFileExistsException.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'Site/exceptions/SiteException.php';
+
+/**
+ * Exception for SiteBotrMediaCommandLine applications when trying to download
+ * to a file that already exists on disk.
+ *
+ * @package   Site
+ * @copyright 2013 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteBotrMediaCommandLineFileExistsException
+	extends SiteBotrMediaToasterException
+{
+}
+
+?>
