aa236a0d593321a7032df787c6aa47cd54ad5513
Michael Gauthier <mike@silverorange.com>
2013-09-19 17:12:39 -0300

    Improve connection handling in Redis module.
    Now does a lazy connect and maintains connected/disconnected status when
    running commands.

--- Site/SiteRedisModule.php
+++ Site/SiteRedisModule.php
@@ -12,30 +12,30 @@ require_once 'Site/exceptions/SiteException.php';
  */
 class SiteRedisModule extends SiteApplicationModule
 {
+	// {{{ protected properties
+
+	/**
+	 * @var Redis
+	 */
+	protected $redis = null;
+
+	/**
+	 * @var boolean
+	 */
+	protected $connected = false;
+
+	// }}}
 	// {{{ public function init()
 
 	public function init()
 	{
 		if (!extension_loaded('redis')) {
-			throw new SiteException('Redis module requires the redis '.
-				'extension to be loaded.'
+			throw new SiteException(
+				'Redis module requires the redis extension to be loaded.'
 			);
 		}
 
-		$config = $this->app->config->redis;
-
 		$this->redis = new Redis();
-		$this->redis->connect($config->server);
-
-		if ($config->prefix != '') {
-			$prefix = $config->prefix;
-			if (!preg_match('/:$/', $prefix)) {
-				$prefix.= ':';
-			}
-			$this->redis->setOption(Redis::OPT_PREFIX, $prefix);
-		}
-
-		$this->redis->select($config->database);
 	}
 
 	// }}}
@@ -43,13 +43,50 @@ class SiteRedisModule extends SiteApplicationModule
 
 	public function __call($name, $arguments)
 	{
-		return call_user_func_array(
-			array(
-				$this->redis,
-				$name
-			),
-			$arguments
-		);
+		try {
+			$this->_connect();
+
+			return call_user_func_array(
+				array(
+					$this->redis,
+					$name
+				),
+				$arguments
+			);
+
+		} catch (RedisException $e) {
+			if ($e->getMessage() === 'Connection lost' ||
+				$e->getMessage() === 'Redis server went away' ||
+				$e->getMessage() === 'read error on connection') {
+				$this->connected = false;
+			}
+			throw $e;
+		}
+	}
+
+	// }}}
+	// {{{ protected function _connect()
+
+	protected function _connect()
+	{
+		if (!$this->connected) {
+			$config = $this->app->config->redis;
+			$this->redis->connect($config->server);
+			try {
+				$this->redis->select($config->database);
+
+				if ($config->prefix != '') {
+					$prefix = $config->prefix;
+					if (!preg_match('/:$/', $prefix)) {
+						$prefix.= ':';
+					}
+					$this->redis->setOption(Redis::OPT_PREFIX, $prefix);
+				}
+
+				$this->connected = true;
+			} catch (RedisException $e) {
+			}
+		}
 	}
 
 	// }}}
