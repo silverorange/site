48ef5a0af108629f3e9f2a8288022cec0dd2ece7
Isaac Grant <isaac@silverorange.com>
2014-04-03 12:40:49 -0300

    New base abstract attachment upload class.
    This can be used across all our attachment admins.

---
+++ Site/admin/components/Attachment/UploadStatusServer.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'Site/pages/SiteUploadStatusServer.php';
+
+/**
+ * An XML-RPC server for attachment upload status
+ *
+ * @package   Site
+ * @copyright 2014 silverorange
+ */
+class SiteAttachmentUploadStatusServer extends SiteUploadStatusServer
+{
+}
+
+?>
