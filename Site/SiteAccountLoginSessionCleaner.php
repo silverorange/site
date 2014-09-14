02092ae35ca4d9b765ac7ccdff6406783e7a92e3
Isaac Grant <isaac@silverorange.com>
2014-09-14 20:36:49 -0300

    Use the correct type of which.
    Big up to @charleswaddell for noticing this first in https://github.com/silverorange/hippo/pull/459

--- Site/SiteAccountLoginSessionCleaner.php
+++ Site/SiteAccountLoginSessionCleaner.php
@@ -106,7 +106,7 @@ class SiteAccountLoginSessionCleaner extends SiteCommandLineApplication
 	 * Adds configuration definitions to the config module of this application
 	 *
 	 * @param SiteConfigModule $config the config module of this application to
-	 *                                  witch to add the config definitions.
+	 *                                  which to add the config definitions.
 	 */
 	protected function addConfigDefinitions(SiteConfigModule $config)
 	{
