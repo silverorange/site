72fab3297428f7fc41250c3f20dd878205f2ef25
Charles Waddell <charles@silverorange.com>
2015-03-16 22:00:53 -0300

    Add media movers for HLS conversion.

---
+++ Site/SiteVideoMediaLocalMover.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once 'Site/SiteVideoMediaMover.php';
+
+/**
+ * Application to copy video to the new local directory structure
+ *
+ * @package   Site
+ * @copyright 2015 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteVideoMediaLocalMover extends SiteVideoMediaMover
+{
+	// {{{ protected properties
+
+	protected $file_base;
+
+	// }}}
+	// {{{ public function setFileBase()
+
+	public function setFileBase($file_base)
+	{
+		$this->file_base = $file_base;
+	}
+
+	// }}}
+	// {{{ public function getFileBase()
+
+	public function getFileBase()
+	{
+		if ($this->file_base === null) {
+			throw new SiteException('File base has not been set.');
+		}
+
+		return $this->file_base;
+	}
+
+	// }}}
+	// {{{ protected function hasOldPath()
+
+	protected function getOldPath(SiteMedia $media, $shortname)
+	{
+		return sprintf(
+			'%s/%s/%s/%s',
+			$this->getFileBase(),
+			$media->media_set->shortname,
+			$shortname,
+			$this->getOldFilename($media, $shortname)
+		);
+	}
+
+	// }}}
+	// {{{ protected function hasNewPath()
+
+	protected function getNewPath(SiteMedia $media, $shortname)
+	{
+		return sprintf(
+			'%s/%s/full/%s',
+			$this->getFileBase(),
+			$media->id,
+			$this->getNewFilename($media, $shortname)
+		);
+	}
+
+	// }}}
+	// {{{ protected function hasFile()
+
+	protected function hasFile($path)
+	{
+		return is_file($path);
+	}
+
+	// }}}
+	// {{{ protected function moveFile()
+
+	protected function moveFile($old_path, $new_path)
+	{
+		rename($old_path, $new_path);
+	}
+
+	// }}}
+}
+
+?>
