249f54501e849f8907f77472764f63ec269e914b
Nick Burka <nick@silverorange.com>
2015-05-05 15:45:38 -0300

    Remove HLS encoding. It's going to Scrunchie

--- sql/pgsql/tables/MediaHLSEncoding.sql
+++
@@ -1,8 +0,0 @@
-create table MediaHLSEncoding (
-	id serial,
-	media_set integer not null references MediaSet(id) on delete cascade,
-	shortname varchar(255),
-	primary key(id)
-);
-
-create index MediaHLSEncoding_shortname_index on MediaHLSEncoding(shortname);
