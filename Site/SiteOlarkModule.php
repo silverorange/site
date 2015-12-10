4391949e59f28ae1c0081b9d7a94fee83e67846d
Nick Burka <nick@silverorange.com>
2015-12-10 09:13:06 -0400

    Olark module

---
+++ Site/SiteOlarkModule.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once 'Site/SiteApplicationModule.php';
+
+/**
+ * Module for dislaying Olark live chat
+ *
+ * @package   Site
+ * @copyright 2015 silverorange
+ * @link      https://www.olark.com/api
+ */
+class SiteOlarkModule extends SiteApplicationModule
+{
+	// {{{ protected properties
+
+	/**
+	 * Olark identity
+	 *
+	 * @var string
+	 */
+	protected $identity;
+
+	// }}}
+	// {{{ public function init()
+
+	public function init()
+	{
+		$config = $this->app->getModule('SiteConfigModule');
+		$this->identity = $config->olark->identity;
+	}
+
+	// }}}
+	// {{{ public function displayScriptTags()
+
+	public function displayScriptTags()
+	{
+		if ($this->identity == '') {
+			throw new SiteException('Olark identity is required');
+		}
+
+		ob_start();
+		?>
+		<script data-cfasync="false" type='text/javascript'>/*<![CDATA[*/window.olark||(function(c){var f=window,d=document,l=f.location.protocol=="https:"?"https:":"http:",z=c.name,r="load";var nt=function(){
+		f[z]=function(){
+		(a.s=a.s||[]).push(arguments)};var a=f[z]._={
+		},q=c.methods.length;while(q--){(function(n){f[z][n]=function(){
+		f[z]("call",n,arguments)}})(c.methods[q])}a.l=c.loader;a.i=nt;a.p={
+		0:+new Date};a.P=function(u){
+		a.p[u]=new Date-a.p[0]};function s(){
+		a.P(r);f[z](r)}f.addEventListener?f.addEventListener(r,s,false):f.attachEvent("on"+r,s);var ld=function(){function p(hd){
+		hd="head";return["<",hd,"></",hd,"><",i,' onl' + 'oad="var d=',g,";d.getElementsByTagName('head')[0].",j,"(d.",h,"('script')).",k,"='",l,"//",a.l,"'",'"',"></",i,">"].join("")}var i="body",m=d[i];if(!m){
+		return setTimeout(ld,100)}a.P(1);var j="appendChild",h="createElement",k="src",n=d[h]("div"),v=n[j](d[h](z)),b=d[h]("iframe"),g="document",e="domain",o;n.style.display="none";m.insertBefore(n,m.firstChild).id=z;b.frameBorder="0";b.id=z+"-loader";if(/MSIE[ ]+6/.test(navigator.userAgent)){
+		b.src="javascript:false"}b.allowTransparency="true";v[j](b);try{
+		b.contentWindow[g].open()}catch(w){
+		c[e]=d[e];o="javascript:var d="+g+".open();d.domain='"+d.domain+"';";b[k]=o+"void(0);"}try{
+		var t=b.contentWindow[g];t.write(p());t.close()}catch(x){
+		b[k]=o+'d.write("'+p().replace(/"/g,String.fromCharCode(92)+'"')+'");d.close();'}a.P(2)};ld()};nt()})({
+		loader: "static.olark.com/jsclient/loader0.js",name:"olark",methods:["configure","extend","declare","identify"]});
+		/* custom configuration goes here (www.olark.com/documentation) */
+		olark.identify(<?= SwatString::quoteJavaScriptString($this->identity) ?>);/*]]>*/</script>
+		<?php
+	}
+
+	// }}}
+}
+
+?>
