ee148e2eb7db3b59612a00dc627146ad7ab5f815
Michael Gauthier <mike@silverorange.com>
2017-07-03 12:20:42 -0300

    Fix method signature of processActions

--- Site/admin/components/Account/Index.php
+++ Site/admin/components/Account/Index.php
@@ -56,7 +56,7 @@ class SiteAccountIndex extends AdminSearch
 	// process phase
 	// {{{ protected function processActions()
 
-	protected function processActions(SwatTableView $view, SwatActions $actions)
+	protected function processActions(SwatView $view, SwatActions $actions)
 	{
 		$num = count($view->getSelection());
 
--- Site/admin/components/Ad/Index.php
+++ Site/admin/components/Ad/Index.php
@@ -32,7 +32,7 @@ class SiteAdIndex extends AdminIndex
 	// process phase
 	// {{{ protected function processActions()
 
-	protected function processActions(SwatTableView $view, SwatActions $actions)
+	protected function processActions(SwatView $view, SwatActions $actions)
 	{
 		$num = count($view->getSelection());
 
