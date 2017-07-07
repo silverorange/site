ddcdb60e2b6f8f7e58b1d765ba0ec7af44dfbc34
Michael Gauthier <mike@silverorange.com>
2017-07-07 17:19:36 -0300

    Use HEREDOC where we use variable interpolation

--- Site/templates/SiteDefaultTemplate.php
+++ Site/templates/SiteDefaultTemplate.php
@@ -11,7 +11,7 @@ class SiteDefaultTemplate implements SiteTemplateInterface
 
 	public function display(SiteLayoutData $data)
 	{
-		echo <<<'HTML'
+		echo <<<HTML
 <!DOCTYPE html>
 <!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
 <!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
