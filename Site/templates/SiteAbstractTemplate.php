7216bbbb41722b891501e57261f1abb527606af7
Michael Gauthier <mike@silverorange.com>
2017-07-11 15:19:50 -0300

    Declare abstract class abstract

--- Site/templates/SiteAbstractTemplate.php
+++ Site/templates/SiteAbstractTemplate.php
@@ -5,7 +5,7 @@
  * @copyright 2017 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class SiteAbstractTemplate implements SiteTemplateInterface
+abstract class SiteAbstractTemplate implements SiteTemplateInterface
 {
 	// {{{ public function asString()
 
