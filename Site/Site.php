56b1b3dc0628257186998c34ac355d6b84b4098b
Kendra Kohler <kendra@roble.grove.silverorange.com>
2018-10-03 19:38:11 -0300

    adding shortname value

--- Site/Site.php
+++ Site/Site.php
@@ -190,6 +190,9 @@ class Site
 			// in favor of 'resources.tag'.
 			'site.resource_tag'        => null,
 
+			// Shortname for the site
+			'site.shortname'           => null
+
 			// Resource tag, used for uncaching html-head-entries.
 			'resources.tag'            => null,
 
