63ee3a048dcd5ddf5e01ed7eb54f39d44b73b1a5
Micah Wittman <micah@wittman.org>
2022-06-17 09:26:15 -0600

    Fix private final function: Remove final modifier from all (except __construct) private methods per php8 compatibility. private final function foo() to private function foo()

--- Site/pages/SiteArticlePage.php
+++ Site/pages/SiteArticlePage.php
@@ -254,7 +254,7 @@ class SiteArticlePage extends SitePathPage
 	}
 
 	// }}}
-	// {{{ private final function getReplacementMarkerTextByMatches()
+	// {{{ private function getReplacementMarkerTextByMatches()
 
 	/**
 	 * Gets replacement text for a replacement marker from within a matches
@@ -265,7 +265,7 @@ class SiteArticlePage extends SitePathPage
 	 * @return string the replacement text for the first parenthesized
 	 *                 subpattern of the <i>$matches</i> array.
 	 */
-	private final function getReplacementMarkerTextByMatches($matches)
+	private function getReplacementMarkerTextByMatches($matches)
 	{
 		if (isset($matches[1]))
 			return $this->getReplacementMarkerText($matches[1]);
