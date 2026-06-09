dd257745a4cf43d9e5cb1fd070f4cedf43bd4ce1
Michael Gauthier <mike@silverorange.com>
2026-06-09 15:51:36 -0300

    Make unit tests for SiteConfigModule do some testing

--- tests/Site/SiteConfigModuleTest.php
+++ tests/Site/SiteConfigModuleTest.php
@@ -3,17 +3,21 @@
 declare(strict_types=1);
 
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\Attributes\After;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @internal
- *
- * @coversNothing
  */
+#[CoversClass(SiteConfigModule::class)]
 class SiteConfigModuleTest extends TestCase
 {
     use MockeryPHPUnitIntegration;
+
+    /** @var string[] */
+    private array $temp_ini_files = [];
 
     #[Test]
     public function usesDefaultsWhenConfigFileIsEmpty(): void
@@ -24,15 +28,15 @@ class SiteConfigModuleTest extends TestCase
         $module->addDefinitions([
             'account.persistent_login_time'    => 2419200,
             'account.persistent_login_enabled' => false,
-            'account.restore_cookie_enabled'   => false,
             'site.meta_description'            => null,
-            'site.title'                       => null,
-            'site.resource_tag'                => null,
-            'site.shortname'                   => null,
+            'site.title'                       => 'default title',
         ]);
-        $module->load($config_filename);
+        $module->load($this->getTempIniFile(''));
 
-        self::assertEquals('test', (string) $module);
+        self::assertEquals(2419200, $module->account->persistent_login_time);
+        self::assertFalse($module->account->persistent_login_enabled);
+        self::assertEquals(null, $module->site->meta_description);
+        self::assertEquals('default title', $module->site->title);
     }
 
     #[Test]
@@ -44,12 +48,44 @@ class SiteConfigModuleTest extends TestCase
         $module->addDefinitions([
             'account.persistent_login_time'    => 2419200,
             'account.persistent_login_enabled' => false,
-            'account.restore_cookie_enabled'   => false,
             'site.meta_description'            => null,
-            'site.title'                       => null,
-            'site.resource_tag'                => null,
-            'site.shortname'                   => null,
+            'site.title'                       => 'default title',
         ]);
-        $module->load($config_filename);
+        $module->load($this->getTempIniFile(
+            <<<'INI'
+                [account]
+                persistent_login_time = 123456;
+                persistent_login_enabled = On;
+
+                [site]
+                meta_description = "Meta description";
+                title = "Title";
+                INI
+        ));
+
+        self::assertEquals(123456, $module->account->persistent_login_time);
+        self::assertEquals('1', $module->account->persistent_login_enabled);
+        self::assertEquals('Meta description', $module->site->meta_description);
+        self::assertEquals('Title', $module->site->title);
+    }
+
+    #[After]
+    public function cleanUpTempFiles(): void
+    {
+        foreach ($this->temp_ini_files as $filename) {
+            unlink($filename);
+        }
+
+        $this->temp_ini_files = [];
+    }
+
+    private function getTempIniFile(string $ini_contents): string
+    {
+        $filename = tempnam(sys_get_temp_dir(), 'ini_test_');
+        file_put_contents($filename, $ini_contents);
+
+        $this->temp_ini_files[] = $filename;
+
+        return $filename;
     }
 }
